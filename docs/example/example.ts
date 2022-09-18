76094d3af5066f5b70ad537ee39e72aae35d1cb1
Henry Heino <henryuheino@gmail.com>
2022-09-18 00:47:08 -0700

    Partial cleanup of example app

--- docs/example/example.ts
+++ docs/example/example.ts
@@ -1,13 +1,14 @@
 // To test importing from a parent directory
 import Editor from '../../src/Editor';
 import '../../src/styles';
+import { showSavePopup, startVisualErrorLog } from './util';
 
 // To test the NPM package
 //import Editor from 'js-draw';
 //import 'js-draw/styles';
 
 // Key in window.localStorage to save the SVG as.
-const saveLocalStorageKey = 'lastSave';
+export const saveLocalStorageKey = 'lastSave';
 
 const createEditor = (saveCallback: ()=>void): Editor => {
 	const parentElement = document.body;
@@ -26,264 +27,10 @@ const createEditor = (saveCallback: ()=>void): Editor => {
 	return editor;
 };
 
-// Saves [editor]'s content as an SVG and displays the result.
-const showSavePopup = (editor: Editor) => {
-	const popupContainer = document.createElement('div');
-	popupContainer.classList.add('popupContainer');
-	popupContainer.appendChild(document.createTextNode('Saving...'));
-	document.body.appendChild(popupContainer);
-
-	const popupIframe = document.createElement('iframe');
-	popupIframe.src = 'about:blank';
-
-	// Turn on sandboxing -- the user may attempt to display the SVG directly in the sandbox.
-	// Allow-scripts is required by Safari on iOS to have click listeners on buttons.
-	// TODO: Check whether this is still the case and remove allow-scripts if no longer necessary.
-	popupIframe.setAttribute('sandbox', 'allow-same-origin allow-downloads allow-scripts');
-	popupIframe.setAttribute('csp', 'default-src \'about:blank\'');
-	popupContainer.replaceChildren(popupIframe);
-
-	const popup = popupIframe.contentWindow!;
-	const popupDoc = popupIframe.contentDocument!;
-
-	if (popup === null) {
-		throw new Error('Unable to open save popup!');
-	}
-
-	const img = editor.toSVG();
-	const imgHTML = img.outerHTML;
-
-	popupDoc.open();
-	popupDoc.write(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta name='viewport' content='initial-scale=1.0'/>
-			<meta charset='utf-8'/>
-		</head>
-		<body>
-			<style>
-				#previewRegion {
-					display: flex;
-					flex-direction: row;
-				}
-
-				svg {
-					height: auto;
-					width: auto;
-					max-width: 500px;
-					max-height: 100vh;
-					flex-grow: 1;
-					border: 1px solid gray;
-				}
-
-				textarea {
-					flex-grow: 1;
-					flex-shrink: 1;
-					width: auto;
-				}
-
-				main {
-					border: 1px solid gray;
-					background-color: #eee;
-					font-family: sans-serif;
-
-					max-width: 600px;
-					margin-left: auto;
-					margin-right: auto;
-
-					padding: 15px;
-					border-radius: 15px;
-				}
-
-				#controlsArea {
-					display: flex;
-					flex-direction: row;
-					margin-top: 14px;
-				}
-
-				#controlsArea button {
-					flex-grow: 1;
-				}
-
-				body, :root {
-					background-color: rgba(255, 255, 255, 0.0);
-				}
-
-				button {
-					min-height: 35px;
-				}
-
-				@media (prefers-color-scheme: dark) {
-					body, :root {
-						background-color: rgba(0, 0, 0, 0.0);
-						color: white;
-					}
-
-					main {
-						background-color: #333;
-					}
-
-					a {
-						color: pink;
-					}
-
-					input, button {
-						background-color: #444;
-						color: #eee;
-						border: 1px solid #222;
-						border-radius: 2px;
-
-						transition: background-color 0.2s ease;
-					}
-
-					button:focus-visible, input:focus-visible {
-						outline: 3px solid #777;
-						background-color: #555;
-					}
-
-					button:active {
-						background-color: #333;
-					}
-				}
-			</style>
-			<main>
-				<p>
-					⚠ Warning ⚠: Some browsers won't save images over 2.5-ish MiB!
-				</p>
-				<div id='previewRegion'>
-					<p>Saving to <code>localStorage</code>...</p>
-				</div>
-				<div><label for='filename'>Download as: </label><input id='filename' type='text' value='editor-save.svg'/></div>
-				<div id='controlsArea'>
-				</div>
-			</main>
-		</body>
-		</html>`
-	);
-	popupDoc.close();
-
-	// Loading the preview can be much slower than saving the image.
-	// Only do so if requested.
-	const previewRegion = popupDoc.querySelector('#previewRegion')!;
-	const previewButton = popupDoc.createElement('button');
-	previewButton.innerText = 'View generated SVG image';
-	previewButton.onclick = () => {
-		const messageContainer = popupDoc.createElement('p');
-		const svgTextContainer = popupDoc.createElement('textarea');
-
-		const imagePreview = popupDoc.createElementNS(
-			'http://www.w3.org/2000/svg', 'svg'
-		);
-		imagePreview.innerHTML = img.innerHTML;
-		imagePreview.setAttribute('viewBox', img.getAttribute('viewBox') ?? '');
-
-		messageContainer.innerText = 'Preview: ';
-		svgTextContainer.value = imgHTML;
-
-		previewRegion.replaceChildren(
-			messageContainer, svgTextContainer, imagePreview
-		);
-	};
-
-	const filenameInput: HTMLInputElement = popupDoc.querySelector('input#filename')!;
-	const downloadButton = popup.document.createElement('button');
-	downloadButton.innerText = 'Download';
-	downloadButton.onclick = () => {
-		const blob = new Blob([ imgHTML ], { type: 'image/svg' });
-		const objectURL = URL.createObjectURL(blob);
-
-		const link = popup.document.createElement('a');
-		link.href = objectURL;
-		link.innerText = 'Download';
-		// Download as (Ref: https://stackoverflow.com/a/52814195/17055750)
-		link.setAttribute('download', filenameInput.value);
-
-		downloadButton.style.display = 'none';
-		popupControlsArea.appendChild(link);
-		link.click();
-
-		// Release URL (see section on object URLs in
-		// https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications)
-		link.remove();
-		URL.revokeObjectURL(objectURL);
-	};
-
-	filenameInput.oninput = () => {
-		downloadButton.style.display = 'block';
-	};
-
-	const closeButton = popup.document.createElement('button');
-	closeButton.innerText = 'Close';
-	closeButton.onclick = () => {
-		popupContainer.remove();
-	};
-
-	const popupControlsArea = popup.document.querySelector('main > #controlsArea')!;
-	popupControlsArea.appendChild(closeButton);
-	popupControlsArea.appendChild(previewButton);
-	popupControlsArea.appendChild(downloadButton);
-
-
-	let localStorageSaveStatus = 'Unable to save to localStorage. ';
-	if (window.localStorage) {
-		// Save
-		try {
-			window.localStorage.setItem(saveLocalStorageKey, imgHTML);
-			localStorageSaveStatus = 'Saved to localStorage! ';
-		} catch(e) {
-			localStorageSaveStatus = `Error saving to localStorage: ${e} `;
-		}
-	}
-
-	let imageSize = `${Math.round(imgHTML.length / 1024 * 10) / 10} KiB`;
-	if (imgHTML.length > 1024 * 1024) {
-		imageSize = `${Math.round(imgHTML.length / 1024 * 10 / 1024) / 10} MiB`;
-	}
-
-	previewRegion.replaceChildren(
-		popup.document.createTextNode(localStorageSaveStatus),
-		popup.document.createTextNode(
-			`Image size: ${imageSize}.`
-		),
-	);
+const saveImage = (editor: Editor) => {
+	showSavePopup(editor.toSVG());
 };
 
-const startVisualErrorLog = () => {
-	const logArea: HTMLTextAreaElement = document.querySelector('#logOutput')!;
-	logArea.style.display = 'block';
-	logArea.value = `
-If enabled, errors will be logged to this textarea.
-	`;
-
-	const scrollLogToEnd = () => {
-		logArea.value = logArea.value.substring(logArea.value.length - 2000);
-		logArea.scrollTop = logArea.scrollHeight;
-	};
-
-	window.onerror = (evt) => {
-		logArea.value += '\nError thrown: ' + evt + '\n';
-		scrollLogToEnd();
-	};
-	const originalErrFn = console.error;
-	const originalWarnFn = console.warn;
-	const originalLogFn = console.log;
-	console.error = (...data) => {
-		originalErrFn.apply(console, data);
-		logArea.value += '\nError logged: ' + data.join(', ') + '\n';
-		scrollLogToEnd();
-	};
-	console.warn = (...data) => {
-		originalWarnFn.apply(console, data);
-		logArea.value += '\nWarning: ' + data.join(', ') + '\n';
-		scrollLogToEnd();
-	};
-	console.log = (...data) => {
-		originalLogFn.apply(console, data);
-		logArea.value += '\nLog: ' + data.join(', ') + '\n';
-		scrollLogToEnd();
-	};
-};
 
 // PWA file access. At the time of this writing, TypeScript does not recognise window.launchQueue.
 declare let launchQueue: any;
@@ -337,13 +84,15 @@ declare let launchQueue: any;
 		// Create the editor and load files.
 		launchQueue.setConsumer(async ({ files }: { files: any[] }) => {
 			optionsScreen.remove();	
-			const editor = createEditor(() => showSavePopup(editor));
+			const editor = createEditor(() => saveImage(editor));
 
 			if (files && files.length > 0) {
 				for (const file of files) {
 					const blob = await file.getFile();
 					blob.handle = file;
 					const data = blob.text();
+
+					// Load the SVG data
 					editor.loadFromSVG(data);
 				}
 			}
@@ -359,7 +108,7 @@ declare let launchQueue: any;
 			startVisualErrorLog();
 		}
 
-		const editor = createEditor(() => showSavePopup(editor));
+		const editor = createEditor(() => saveImage(editor));
 
 		sourceText ??= textareaData;
 		if (sourceText === loadFromLastSaveText) {
@@ -368,6 +117,9 @@ declare let launchQueue: any;
 
 		if (sourceText && sourceText.length > 0) {
 			editor.loadFromSVG(sourceText);
+
+			// Don't keep sourceText in memory --- it might be quite large.
+			sourceText = '';
 		}
 	};
 })();
---
+++ docs/example/util.ts
@@ -0,0 +1,264 @@
+// Functions that are specific to this particular example (e.g.
+// functions helpful for debugging, etc.)
+
+import { saveLocalStorageKey } from './example';
+
+
+// Log errors, etc. to a visible element. Useful for debugging on mobile devices.
+export const startVisualErrorLog = () => {
+	const logArea: HTMLTextAreaElement = document.querySelector('#logOutput')!;
+	logArea.style.display = 'block';
+	logArea.value = `
+If enabled, errors will be logged to this textarea.
+	`;
+
+	const scrollLogToEnd = () => {
+		logArea.value = logArea.value.substring(logArea.value.length - 2000);
+		logArea.scrollTop = logArea.scrollHeight;
+	};
+
+	window.onerror = (evt) => {
+		logArea.value += '\nError thrown: ' + evt + '\n';
+		scrollLogToEnd();
+	};
+	const originalErrFn = console.error;
+	const originalWarnFn = console.warn;
+	const originalLogFn = console.log;
+	console.error = (...data) => {
+		originalErrFn.apply(console, data);
+		logArea.value += '\nError logged: ' + data.join(', ') + '\n';
+		scrollLogToEnd();
+	};
+	console.warn = (...data) => {
+		originalWarnFn.apply(console, data);
+		logArea.value += '\nWarning: ' + data.join(', ') + '\n';
+		scrollLogToEnd();
+	};
+	console.log = (...data) => {
+		originalLogFn.apply(console, data);
+		logArea.value += '\nLog: ' + data.join(', ') + '\n';
+		scrollLogToEnd();
+	};
+};
+
+// Saves [editor]'s content as an SVG and displays the result.
+export const showSavePopup = (img: SVGElement) => {
+	const imgHTML = img.outerHTML;
+
+	const popupContainer = document.createElement('div');
+	popupContainer.classList.add('popupContainer');
+	popupContainer.appendChild(document.createTextNode('Saving...'));
+	document.body.appendChild(popupContainer);
+
+	const popupIframe = document.createElement('iframe');
+	popupIframe.src = 'about:blank';
+
+	// Turn on sandboxing -- the user may attempt to display the SVG directly in the sandbox.
+	// Allow-scripts is required by Safari on iOS to have click listeners on buttons.
+	// TODO: Check whether this is still the case and remove allow-scripts if no longer necessary.
+	popupIframe.setAttribute('sandbox', 'allow-same-origin allow-downloads allow-scripts');
+	popupIframe.setAttribute('csp', 'default-src \'about:blank\'');
+	popupContainer.replaceChildren(popupIframe);
+
+	const popup = popupIframe.contentWindow!;
+	const popupDoc = popupIframe.contentDocument!;
+
+	if (popup === null) {
+		throw new Error('Unable to open save popup!');
+	}
+
+	popupDoc.open();
+	popupDoc.write(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta name='viewport' content='initial-scale=1.0'/>
+			<meta charset='utf-8'/>
+		</head>
+		<body>
+			<style>
+				#previewRegion {
+					display: flex;
+					flex-direction: row;
+				}
+
+				svg {
+					height: auto;
+					width: auto;
+					max-width: 500px;
+					max-height: 100vh;
+					flex-grow: 1;
+					border: 1px solid gray;
+				}
+
+				textarea {
+					flex-grow: 1;
+					flex-shrink: 1;
+					width: auto;
+				}
+
+				main {
+					border: 1px solid gray;
+					background-color: #eee;
+					font-family: sans-serif;
+
+					max-width: 600px;
+					margin-left: auto;
+					margin-right: auto;
+
+					padding: 15px;
+					border-radius: 15px;
+				}
+
+				#controlsArea {
+					display: flex;
+					flex-direction: row;
+					margin-top: 14px;
+				}
+
+				#controlsArea button {
+					flex-grow: 1;
+				}
+
+				body, :root {
+					background-color: rgba(255, 255, 255, 0.0);
+				}
+
+				button {
+					min-height: 35px;
+				}
+
+				@media (prefers-color-scheme: dark) {
+					body, :root {
+						background-color: rgba(0, 0, 0, 0.0);
+						color: white;
+					}
+
+					main {
+						background-color: #333;
+					}
+
+					a {
+						color: pink;
+					}
+
+					input, button {
+						background-color: #444;
+						color: #eee;
+						border: 1px solid #222;
+						border-radius: 2px;
+
+						transition: background-color 0.2s ease;
+					}
+
+					button:focus-visible, input:focus-visible {
+						outline: 3px solid #777;
+						background-color: #555;
+					}
+
+					button:active {
+						background-color: #333;
+					}
+				}
+			</style>
+			<main>
+				<p>
+					⚠ Warning ⚠: Some browsers won't save images over 2.5-ish MiB!
+				</p>
+				<div id='previewRegion'>
+					<p>Saving to <code>localStorage</code>...</p>
+				</div>
+				<div><label for='filename'>Download as: </label><input id='filename' type='text' value='editor-save.svg'/></div>
+				<div id='controlsArea'>
+				</div>
+			</main>
+		</body>
+		</html>`
+	);
+	popupDoc.close();
+
+	// Loading the preview can be much slower than saving the image.
+	// Only do so if requested.
+	const previewRegion = popupDoc.querySelector('#previewRegion')!;
+	const previewButton = popupDoc.createElement('button');
+	previewButton.innerText = 'View generated SVG image';
+	previewButton.onclick = () => {
+		const messageContainer = popupDoc.createElement('p');
+		const svgTextContainer = popupDoc.createElement('textarea');
+
+		const imagePreview = popupDoc.createElementNS(
+			'http://www.w3.org/2000/svg', 'svg'
+		);
+		imagePreview.innerHTML = img.innerHTML;
+		imagePreview.setAttribute('viewBox', img.getAttribute('viewBox') ?? '');
+
+		messageContainer.innerText = 'Preview: ';
+		svgTextContainer.value = imgHTML;
+
+		previewRegion.replaceChildren(
+			messageContainer, svgTextContainer, imagePreview
+		);
+	};
+
+	const filenameInput: HTMLInputElement = popupDoc.querySelector('input#filename')!;
+	const downloadButton = popup.document.createElement('button');
+	downloadButton.innerText = 'Download';
+	downloadButton.onclick = () => {
+		const blob = new Blob([ imgHTML ], { type: 'image/svg' });
+		const objectURL = URL.createObjectURL(blob);
+
+		const link = popup.document.createElement('a');
+		link.href = objectURL;
+		link.innerText = 'Download';
+		// Download as (Ref: https://stackoverflow.com/a/52814195/17055750)
+		link.setAttribute('download', filenameInput.value);
+
+		downloadButton.style.display = 'none';
+		popupControlsArea.appendChild(link);
+		link.click();
+
+		// Release URL (see section on object URLs in
+		// https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications)
+		link.remove();
+		URL.revokeObjectURL(objectURL);
+	};
+
+	filenameInput.oninput = () => {
+		downloadButton.style.display = 'block';
+	};
+
+	const closeButton = popup.document.createElement('button');
+	closeButton.innerText = 'Close';
+	closeButton.onclick = () => {
+		popupContainer.remove();
+	};
+
+	const popupControlsArea = popup.document.querySelector('main > #controlsArea')!;
+	popupControlsArea.appendChild(closeButton);
+	popupControlsArea.appendChild(previewButton);
+	popupControlsArea.appendChild(downloadButton);
+
+
+	let localStorageSaveStatus = 'Unable to save to localStorage. ';
+	if (window.localStorage) {
+		// Save
+		try {
+			window.localStorage.setItem(saveLocalStorageKey, imgHTML);
+			localStorageSaveStatus = 'Saved to localStorage! ';
+		} catch(e) {
+			localStorageSaveStatus = `Error saving to localStorage: ${e} `;
+		}
+	}
+
+	let imageSize = `${Math.round(imgHTML.length / 1024 * 10) / 10} KiB`;
+	if (imgHTML.length > 1024 * 1024) {
+		imageSize = `${Math.round(imgHTML.length / 1024 * 10 / 1024) / 10} MiB`;
+	}
+
+	previewRegion.replaceChildren(
+		popup.document.createTextNode(localStorageSaveStatus),
+		popup.document.createTextNode(
+			`Image size: ${imageSize}.`
+		),
+	);
+};
