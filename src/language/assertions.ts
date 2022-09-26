3283529a82ba64ba89260b897ccfdb5e54ee0dda
Henry Heino <henryuheino@gmail.com>
2022-09-26 16:41:26 -0700

    Update workflows
    Workflows should now run the linter.

--- src/language/assertions.ts
+++ src/language/assertions.ts
@@ -2,5 +2,5 @@
 // Compile-time assertion that a branch of code is unreachable.
 // See https://stackoverflow.com/a/39419171/17055750
 export const assertUnreachable = (key: never): never => {
-    throw new Error(`Should be unreachable. Key: ${key}.`);
+	throw new Error(`Should be unreachable. Key: ${key}.`);
 };
