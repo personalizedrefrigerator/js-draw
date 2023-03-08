65c3db2c43469122b5b0fa2dc6835a3d16d29af2
Henry Heino <personalizedrefrigerator@gmail.com>
2023-03-08 10:10:57 -0800

    Fix missing rendering mode and style exports

--- src/rendering/lib.ts
+++ src/rendering/lib.ts
@@ -3,4 +3,4 @@ export { default as AbstractRenderer } from './renderers/AbstractRenderer';
 export { default as DummyRenderer } from './renderers/DummyRenderer';
 export { default as SVGRenderer } from './renderers/SVGRenderer';
 export { default as CanvasRenderer } from './renderers/CanvasRenderer';
-export { default as Display } from './Display';
+export { default as Display, RenderingMode } from './Display';
