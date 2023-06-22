795f72e1c0578f5146f4b92d851dbe680591757b
Henry Heino <personalizedrefrigerator@gmail.com>
2023-06-21 17:23:44 -0700

    New shape data structure: Ellipse

---
+++ packages/js-draw/src/math/shapes/Ellipse.test.ts
@@ -0,0 +1,212 @@
+import Mat33 from '../Mat33';
+import { Point2, Vec2 } from '../Vec2';
+import Ellipse from './Ellipse';
+import LineSegment2 from './LineSegment2';
+
+describe('Ellipse', () => {
+	type ProcessEllipseCallback = (
+		ellipse: Ellipse,
+
+		// Foci
+		f1: Point2, f2: Point2,
+
+		// Angle of the ellipse
+		angle: number,
+
+		// t is a parameter that can be used to initialize other variables.
+		t: number
+	)=>void;
+	const forEachEllipseOnMainTestPath = (handleEllipse: ProcessEllipseCallback) => {
+		for (let t = 0; t < 1; t += 0.1) {
+			// Select parameters based on t
+			const x1 = Math.cos(t * 2 * Math.PI) * 10;
+			const y1 = Math.sin(t * 3 * Math.PI) * 3;
+			const dist = (Math.cos(t * 12 * Math.PI) + 1) * 4;
+			const angle = t * 2 * Math.PI;
+			const semimajorAxisLen = 16 + t;
+
+			// Compute the foci locations from the parameters.
+			const x2 = x1 + dist * Math.cos(angle);
+			const y2 = y1 + dist * Math.sin(angle);
+
+			const f1 = Vec2.of(x1, y1);
+			const f2 = Vec2.of(x2, y2);
+
+			const ellipse = new Ellipse(f1, f2, semimajorAxisLen);
+			handleEllipse(ellipse, f1, f2, angle, t);
+		}
+	};
+
+	describe('should compute correct center, angle, and transform', () => {
+		it('for the unit circle', () => {
+			const center = Vec2.zero;
+			const radius = 1;
+			const circle = new Ellipse(center, center, radius);
+			expect(circle.rx).toBeCloseTo(radius);
+			expect(circle.ry).toBeCloseTo(radius);
+			expect(circle.transform).objEq(Mat33.identity);
+			expect(circle.center).objEq(center);
+			expect(circle.angle).toBeCloseTo(0);
+		});
+
+		it('for a circle', () => {
+			for (let x = -1; x < 1; x += 0.3) {
+				for (let y = -1; y < 1; y += 0.3) {
+					const center = Vec2.of(x, y);
+					const ellipse = new Ellipse(center, center, 1);
+					expect(ellipse.center).objEq(center);
+					expect(ellipse.angle).toBeCloseTo(0);
+					expect(ellipse.ry).toBeCloseTo(1);
+
+					// Should also map the unit x and unit y vectors to translated unit x and
+					// unit y vectors (points on the unit circle -> points on the translated unit
+					// circle).
+					expect(
+						ellipse.transform.transformVec2(Vec2.unitX)
+					).objEq(ellipse.center.plus(Vec2.unitX));
+					expect(
+						ellipse.transform.transformVec2(Vec2.unitY)
+					).objEq(ellipse.center.plus(Vec2.unitY));
+				}
+			}
+		});
+
+		it('for an ellipse on the x axis', () => {
+			for (let x1 = -1; x1 < 2; x1 += 0.3) {
+				for (let x2 = -2; x2 < 1; x2 += 0.3) {
+					const f1 = Vec2.of(x1, 0);
+					const f2 = Vec2.of(x2, 0);
+
+					const ellipse = new Ellipse(f1, f2, 12);
+					expect(ellipse.center).objEq(Vec2.of((f1.x + f2.x) / 2, 0));
+
+					// Should produce an angle that results in a line with 0 slope
+					expect(Math.tan(ellipse.angle)).toBeCloseTo(0);
+
+					expect(
+						ellipse.hasPointOnBoundary(ellipse.transform.transformVec2(Vec2.unitX))
+					).toBe(true);
+					expect(
+						ellipse.hasPointOnBoundary(ellipse.transform.transformVec2(Vec2.unitY))
+					).toBe(true);
+				}
+			}
+		});
+
+		it('for a rotated ellipse', () => {
+			forEachEllipseOnMainTestPath((ellipse, f1, f2, angle, t) => {
+				expect(ellipse.center).objEq(f1.plus(f2).times(0.5));
+
+				// Angle should produce the same line as ellipse.angle
+				expect(Math.abs(Math.cos(ellipse.angle))).toBeCloseTo(Math.abs(Math.cos(angle)));
+				expect(Math.abs(Math.sin(ellipse.angle))).toBeCloseTo(Math.abs(Math.sin(angle)));
+
+				// Should also transform a point from the unit circle to this ellipse
+				const circlePoint = Vec2.of(Math.cos(t * 2 * Math.PI), Math.sin(t * 2 * Math.PI));
+				const ellipsePoint = ellipse.transform.transformVec2(circlePoint);
+				expect(ellipse.hasPointOnBoundary(ellipsePoint)).toBe(true);
+			});
+		});
+	});
+
+	describe('should compute correct signed distance', () => {
+		it('for a circle', () => {
+			for (const center of [ Vec2.zero, Vec2.of(1, 0.5)]) {
+				const radius = 1;
+				const circle = new Ellipse(center, center, radius);
+
+				expect(
+					circle.signedDistance(Vec2.of(2, 0).plus(center))
+				).toBeCloseTo(2 - radius);
+				expect(
+					circle.signedDistance(Vec2.of(-1, -1).plus(center))
+				).toBeCloseTo(Math.hypot(1 - 1/Math.SQRT2, 1 - 1/Math.SQRT2));
+				expect(circle.signedDistance(center)).toBeCloseTo(-radius);
+			}
+		});
+
+		it('for a rotated ellipse', () => {
+			forEachEllipseOnMainTestPath((ellipse, _f1, _f2, _angle, t) => {
+				// Should return a negative signed distance for points within
+				// the ellipse
+				const delta = Vec2.of(
+					ellipse.rx * Math.cos(t) * 0.9, ellipse.ry * Math.sin(t) * 0.9
+				);
+				expect(ellipse.signedDistance(ellipse.center.plus(delta))).toBeLessThan(0);
+				expect(ellipse.signedDistance(ellipse.center)).toBeLessThan(0);
+
+				// Should return a positive signed distance for points outside of the ellipse.
+				expect(
+					ellipse.signedDistance(ellipse.center.plus(delta.times(2)))
+				).toBeGreaterThan(0);
+
+				// Should return zero for points on the ellipse,
+				// length of normal for points translated by normal.
+				for (let t2 = 0; t2 < 2 * Math.PI; t2 += 0.3) {
+					expect(ellipse.hasPointOnBoundary(ellipse.at(t2))).toBe(true);
+					expect(
+						ellipse.signedDistance(ellipse.at(t2))
+					).toBeCloseTo(0);
+
+					// Shifting along the normal should produce the distance
+					// gone along the normal.
+					let normalDist = Math.tan(t2 + 0.1);
+
+					if (Math.abs(normalDist) > Math.min(ellipse.rx, ellipse.ry)) {
+						normalDist = Math.min(ellipse.rx, ellipse.ry) * Math.sin(t2 + 0.1);
+					}
+
+					const normal = ellipse.derivativeAt(t2).orthog().normalized();
+					const p2 = ellipse.at(t2).plus(normal.times(-normalDist));
+
+					expect(
+						ellipse.signedDistance(p2)
+					).toBeCloseTo(normalDist);
+				}
+			});
+		});
+	});
+
+	describe('should compute line segment intersection correctly', () => {
+		it('for a circle', () => {
+			const center = Vec2.zero;
+			const radius = 1;
+			const circle = new Ellipse(center, center, radius);
+
+			const seg1 = new LineSegment2(Vec2.of(-10, 0), Vec2.of(10, 0));
+			const intersections1 = circle.intersectsLineSegment(seg1);
+			expect(intersections1).toHaveLength(2);
+			expect(intersections1[0]).objEq(Vec2.of(1, 0));
+			expect(intersections1[1]).objEq(Vec2.of(-1, 0));
+
+			const seg2 = new LineSegment2(Vec2.of(0, -10), Vec2.of(0, 10));
+			const intersections2 = circle.intersectsLineSegment(seg2);
+			expect(intersections2).toHaveLength(2);
+			expect(intersections2[0]).objEq(Vec2.of(0, 1));
+			expect(intersections2[1]).objEq(Vec2.of(0, -1));
+		});
+	});
+
+	describe('should compute correct tight bounding box', () => {
+		it('for a circle', () => {
+			for (const center of [ Vec2.zero, Vec2.of(1, 0.5)]) {
+				const radius = 1;
+				const circle = new Ellipse(center, center, radius);
+
+				const bbox = circle.getTightBoundingBox();
+				expect(bbox.size).objEq(Vec2.of(radius * 2, radius * 2));
+				expect(bbox.center).objEq(center);
+			}
+		});
+
+		it('for a stretched circle', () => {
+			const rx = 2;
+			const stretchedCircle = new Ellipse(Vec2.of(-1, 0), Vec2.of(1, 0), rx);
+
+			const bbox = stretchedCircle.getTightBoundingBox();
+			expect(bbox.center).objEq(Vec2.zero);
+			expect(bbox.width).toBe(4); // 2rx
+			expect(bbox.height).toBe(2 * Math.sqrt(rx ** 2 - 1 ** 2)); // 2ry
+		});
+	});
+});
---
+++ packages/js-draw/src/math/shapes/Ellipse.ts
@@ -0,0 +1,349 @@
+import Mat33 from '../Mat33';
+import { Point2, Vec2 } from '../Vec2';
+import Vec3 from '../Vec3';
+import solveQuadratic from '../polynomial/solveQuadratic';
+import Abstract2DShape from './Abstract2DShape';
+import LineSegment2 from './LineSegment2';
+import Rect2 from './Rect2';
+
+/**
+ * We define an ellipse with two foci, `f1` and `f2`, and a distance, `rx`.
+ *
+ * The ellipse is the set `{ p ∈ ℝ² : ‖f1 - p‖ + ‖f2 - p‖ = 2rx }`.
+ *
+ * We call `rx` the **semimajor axis** and `2rx` the **major axis**.
+ */
+class Ellipse extends Abstract2DShape {
+	/** Center of the ellipse: The point between its two foci. */
+	public readonly center: Point2;
+
+	/** Length of the semiminor axis */
+	public readonly ry: number;
+
+	/** Angle the ellipse is rotated (counter clockwise, radians). */
+	public readonly angle: number;
+
+	/**
+	 * Transformation from the unit circle to this ellipse. As such,
+	 *     `this.transform @ point`
+	 * for `point` on the unit circle produces a point on this ellipse.
+	 */
+	public readonly transform: Mat33;
+
+
+	/**
+	 * Creates a new Ellipse ([about ellipses](https://mathworld.wolfram.com/Ellipse.html)).
+	 *
+	 * @param f1 Position of the first focus
+	 * @param f2 Position of the second focus
+	 * @param a Length of the semimajor axis
+	 */
+	public constructor(
+		// Focus 1
+		public readonly f1: Point2,
+
+		// Focus 2
+		public readonly f2: Point2,
+
+		// Length of the semimajor axis
+		public readonly rx: number,
+	) {
+		super();
+
+		// The center is halfway between f1 and f2.
+		this.center = f1.lerp(f2, 0.5);
+
+		// With a point on the (rotated) y axis, the lines from the foci to the
+		// point form two symmetric triangles:
+		//       /|\
+		//      / | \
+		//     /  |ry\
+		//    /   |   \
+		//  f1▔▔▔▔▔▔▔▔▔f2
+		//      l    l
+		//
+		// l² + ry² = a² ⟹ ry² = a² - l².
+		//
+		// Because the point is on the ellipse,
+		//    a + a = 2rx ⟹ a = rx.
+		// Thus,
+		//    ry² = rx² - l²
+		const l = this.center.minus(f1).length();
+		this.ry = Math.sqrt(this.rx * this.rx - l * l);
+
+		// Angle between the x-axis in the XY plane and the major axis of this ellipse.
+		this.angle = this.f2.minus(this.center).angle();
+
+		// Transforms a point on the unit circle to this ellipse.
+		this.transform = Mat33.translation(this.center).rightMul(
+			Mat33.zRotation(this.angle)
+		).rightMul(
+			Mat33.scaling2D(Vec2.of(this.rx, this.ry))
+		);
+	}
+
+	/** @returns a point on this ellipse, given a parameter value, `t ∈ [0, 2π]`. */
+	public at(t: number) {
+		// See https://math.stackexchange.com/q/2645689
+
+		// Start with an unrotated ellipse:
+		//   v₀(t) = (rx * cos(t), ry * sin(t))
+		// Rotate it:
+		//
+		//            ((cos ϑ, -sin ϑ)
+		//   v₁(t) =   (sin ϑ,  cos ϑ))  *  v₀(t)
+		//
+		//         = (rx (cos ϑ)(cos t) - ry (sin ϑ)(sin t),
+		//            rx (sin ϑ)(cos t) + ry (sin t)(cos ϑ)).
+		//
+		// Finally, center it:
+		//   v(t) = v₁(t) + center
+		const cosAngle = Math.cos(this.angle);
+		const sinAngle = Math.sin(this.angle);
+		const sint = Math.sin(t);
+		const cost = Math.cos(t);
+		return Vec2.of(
+			this.rx * cosAngle * cost - this.ry * sint * sinAngle,
+			this.rx * sinAngle * cost + this.ry * sint * cosAngle,
+		).plus(this.center);
+	}
+
+	public derivativeAt(t: number) {
+		const cosAngle = Math.cos(this.angle);
+		const sinAngle = Math.sin(this.angle);
+		const sint = Math.sin(t);
+		const cost = Math.cos(t);
+		return Vec2.of(
+			this.rx * cosAngle * (-sint) - this.ry * cost * sinAngle,
+			this.rx * sinAngle * (-sint) + this.ry * cost * cosAngle
+		);
+	}
+
+	public override getTightBoundingBox(): Rect2 {
+		const cosAngle = Math.cos(this.angle);
+		const sinAngle = Math.sin(this.angle);
+
+		// See https://www.desmos.com/calculator/dpj1wrif16
+		//
+		// At extrema, derivativeAt(t) has a zero x or y component. Thus,
+		// for angle = α, yComponent(t) = 0 and thus,
+		//      rx * sin(α) * (-sin t) + ry * cos t * cos(α) = 0
+		//  ⟹ rx (sin α) (sin t) = ry (cos t) (cos α)
+		//  ⟹ (sin t)         = (ry (cos t)(cos α)) / (rx (sin α))
+		//  ⟹ (sin t)/(cos t) = (ry (cos α)) / (rx (sin α))
+		//  ⟹            t    = Arctan2(ry (cos α), rx (sin α)) ± 2πk
+		const yExtremaT = Math.atan2(this.ry * cosAngle, this.rx * sinAngle);
+		const yExtrema1 = this.at(yExtremaT);
+		const yExtrema2 = this.at(yExtremaT + Math.PI);
+
+		// Similarly, for angle α, xComponent(t) = 0, thus,
+		//     rx (cos α) (-sin t) - ry (sin α) (cos t) = 0
+		// ⟹  rx (cos α) (-sin t) = ry (sin α) (cos t)
+		// ⟹          -sin t      = ry (cos t) (sin α) / (rx (cos α))
+		// ⟹     -(sin t)/(cos t) = ry (sin α) / (rx cos α)
+		// ⟹             tan t    = -ry (sin α) / (rx cos α)
+		const xExtremaT = Math.atan2(-this.ry * sinAngle, this.rx * cosAngle);
+		const xExtrema1 = this.at(xExtremaT);
+		const xExtrema2 = this.at(xExtremaT + Math.PI);
+
+		return Rect2.bboxOf([ xExtrema1, xExtrema2, yExtrema1, yExtrema2 ]);
+	}
+
+
+	/**
+	 * Returns the points (if any) at which the line containing `line` intersects
+	 * this.
+	 */
+	private lineIntersections(line: LineSegment2): [Point2, Point2]|null {
+		// Convert the segment into something in the space of the unit circle for
+		// easier math.
+		const segInCircleSpace = line.transformedBy(this.transform.inverse());
+
+		// Intersect segInCircleSpace with the unit circle:
+		//
+		// The unit circle satisfies x² + y² = 1 for all x, y in the circle.
+		// Thus, where the line segment intersects the circle,
+		//        1 = (x(t))² + (y(t))²
+		//          = (x₀ + vₓt)² + (y₀ + vᵧt)²      <-- for v = segInCircleSpace.direction
+		//          = x₀² + 2x₀vₓt + vₓ²t² + y₀² + 2y₀vᵧt + vᵧ²t²
+		//          = vₓ²t² + vᵧ²t² + 2x₀vₓt + 2y₀vᵧt + x₀² + y₀²
+		//    ⟹ 0 = (t²)(vₓ² + vᵧ²) + (t)(2x₀vₓ + 2y₀vᵧ) + x₀² + y₀² - 1
+		// Hence, we solve a quadratic for t:
+		const v = segInCircleSpace.direction;
+		const p1 = segInCircleSpace.p1;
+		const a = v.x * v.x + v.y * v.y;
+		const b = 2 * p1.x * v.x + 2 * p1.y * v.y;
+		const c = p1.x * p1.x + p1.y * p1.y - 1;
+
+		const [ lineParam1, lineParam2 ] = solveQuadratic(a, b, c);
+
+		// No solutions?
+		if (isNaN(lineParam1) || isNaN(lineParam2)) {
+			return null;
+		}
+
+		// Turn the line parameters into points:
+		const sol1 = p1.plus(v.times(lineParam1));
+		const sol2 = p1.plus(v.times(lineParam2));
+
+		// Return solutions transformed back
+		return [
+			this.transform.transformVec2(sol1),
+			this.transform.transformVec2(sol2),
+		];
+	}
+
+	/**
+	 * Returns the (at most two) parameter values for the intersection of the line containing
+	 * the given segment with this.
+	 *
+	 * This returns two (possibly the same) parameter values, or null
+	 * if the line containing `line` does not intersect this.
+	 */
+	private parmeterForLineIntersection(line: LineSegment2): [number, number]|null {
+		const points = this.lineIntersections(line);
+
+		if (!points) {
+			return null;
+		}
+
+		const invTransform = this.transform.inverse();
+		const unitCirclePoint1 = invTransform.transformVec2(points[0]);
+		const unitCirclePoint2 = invTransform.transformVec2(points[1]);
+
+		const param1 = unitCirclePoint1.angle();
+		const param2 = unitCirclePoint2.angle();
+
+		// Parameters remain the same under transformation
+		return [ param1, param2 ];
+	}
+
+	public override intersectsLineSegment(lineSegment: LineSegment2): Point2[] {
+		const intersectionsOnLine = this.lineIntersections(lineSegment);
+
+		// No intersections
+		if (intersectionsOnLine === null) {
+			return [];
+		}
+
+		const intersectionsOnSeg = intersectionsOnLine
+			.filter(p => lineSegment.containsPoint(p));
+
+		if (intersectionsOnSeg.length === 2) {
+			if (intersectionsOnSeg[0].eq(intersectionsOnSeg[1])) {
+				return [ intersectionsOnSeg[0] ];
+			}
+		}
+
+		return intersectionsOnSeg;
+	}
+
+	/**
+	 * Returns the distance from the edge of this to `point`, or, if `point` is inside this,
+	 * the negative of that distance.
+	 */
+	public override signedDistance(point: Vec3): number {
+		// Our initial guess below requires that point ≠ center. Thus, we need
+		// an edge case.
+		if (point.eq(this.center)) {
+			return -Math.min(this.rx, this.ry);
+		}
+
+		// Letting p be `point` and v(t) be a point on the ellipse given parameter t, observe that
+		//    f(t) = ‖v(t) - p‖²
+		// is the square distance from the point on the ellipse at parameter value t and p.
+		//
+		// Thus, letting q = p - center,
+		//    f(t) = ‖(rx (cos ϑ)(cos t) - ry (sin ϑ)(sin t) + center.x - pₓ,
+		//            rx (sin ϑ)(cos t) + ry (sin t)(cos ϑ) + center.y - pᵧ)‖²
+		//         = ‖(rx (cos ϑ)(cos t) - ry (sin ϑ)(sin t) - qₓ,
+		//            rx (sin ϑ)(cos t) + ry (sin t)(cos ϑ) - qᵧ)‖²
+		//         = (rx (cos ϑ)(cos t) - ry (sin ϑ)(sin t) - qₓ)²
+		//           + (rx (sin ϑ)(cos t) + ry (sin t)(cos ϑ) - qᵧ)².
+		//
+		// Hence,
+		//   f'(t) = 2(rx (cos ϑ)(cos t) - ry (sin ϑ)(sin t) - qₓ)
+		//                  (rx (cos ϑ)(-sin t) - ry (sin ϑ)(cos t))
+		//           + 2(rx (sin ϑ)(cos t) + ry (sin t)(cos ϑ) - qᵧ)
+		//                  (rx (sin ϑ)(-sin t) + ry (cos t)(cos ϑ))
+		//         = 2 Δₓ(t) (rx (cos ϑ)(-sin t) - ry (sin ϑ)(cos t))
+		//           + 2 Δᵧ(t) (rx (sin ϑ)(-sin t) + ry (cos t)(cos ϑ))
+		//         = 2 Δₓ(t) rx (cos ϑ)(-sin t) - 2 Δₓ(t) ry (sin ϑ)(cos t))
+		//           + 2 Δᵧ(t) rx (sin ϑ)(-sin t) + 2 Δᵧ(t) ry (cos t)(cos ϑ)
+		// where Δ(t) = v(t) - p
+		//
+		// We want to find the t that minimizes the square distance. We can do this using gradient
+		// descent.
+		const loss = (t: number) => {
+			return this.at(t).minus(point).magnitudeSquared();
+		};
+
+		const lossDerivative = (t: number) => {
+			const v = this.at(t).minus(point);
+
+			//  2 vₓ(t) (rx (cos ϑ)(-sin t) - ry (sin ϑ)(cos t))
+			//+ 2 vᵧ(t) (rx (sin ϑ)(-sin t) + ry (cos t)(cos ϑ))
+			const cost = Math.cos(t);
+			const sint = Math.sin(t);
+			const cosTheta = Math.cos(this.angle);
+			const sinTheta = Math.sin(this.angle);
+			return 2 * v.x * (this.rx * cosTheta * (-sint) - this.ry * sinTheta * cost)
+				+ 2 * v.y * (this.rx * sinTheta * (-sint) + this.ry * cost * cosTheta);
+		};
+
+		const [ guess1, guess2 ] =
+			this.parmeterForLineIntersection(new LineSegment2(this.center, point)) ?? [ 0, Math.PI ];
+
+		// Choose the guess that produces the smallest loss
+		const guess = loss(guess1) < loss(guess2) ? guess1 : guess2;
+
+		// Gradient descent with 10 steps, by default:
+		let steps = 10;
+		// ... but no more than 100 steps.
+		const maxSteps = 100;
+		let learningRate = 1.5;
+		let tApproximation = guess;
+		let lastLoss = loss(tApproximation);
+		for (let i = 0; i < steps && i < maxSteps; i ++) {
+			const direction = lossDerivative(tApproximation);
+			const newApproximation = tApproximation - learningRate * direction;
+
+			const newLoss = loss(tApproximation);
+			if (lastLoss >= newLoss) {
+				tApproximation = newApproximation;
+			} else {
+				// Adjust the learning rate if necessary
+				learningRate /= 10;
+
+				// Take more steps to compensate for the smaller learning rate.
+				steps += 4;
+			}
+
+			lastLoss = newLoss;
+		}
+
+		const dist = this.at(tApproximation).minus(point).length();
+
+		// SDF is negative for points contained in the ellipse.
+		if (this.containsPoint(point)) {
+			return -dist;
+		}
+		return dist;
+	}
+
+	public override containsPoint(point: Vec3, epsilon: number = 0): boolean {
+		const distSum = this.f1.minus(point).magnitude() + this.f2.minus(point).magnitude();
+		return distSum <= 2 * this.rx + epsilon;
+	}
+
+	public hasPointOnBoundary(point: Vec3, epsilon: number = Abstract2DShape.smallValue) {
+		const distSum = this.f1.minus(point).magnitude() + this.f2.minus(point).magnitude();
+		return distSum <= 2 * this.rx + epsilon && 2 * this.rx - epsilon <= distSum;
+	}
+
+	public override toString() {
+		return `Ellipse { rx: ${this.rx}, f1: ${this.f1}, f2: ${this.f2} }`;
+	}
+}
+
+export default Ellipse;
