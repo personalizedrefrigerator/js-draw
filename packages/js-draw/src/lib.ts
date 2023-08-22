338ac6766f59881a88e05d740242464f6232b7fc
Henry Heino <personalizedrefrigerator@gmail.com>
2023-08-22 11:35:22 -0700

    Use the MaterialIconProvider in the default example

--- packages/js-draw/src/lib.ts
+++ packages/js-draw/src/lib.ts
@@ -6,13 +6,18 @@
  * ```ts,runnable
  * import { Editor, Vec3, Mat33, ToolbarWidgetTag } from 'js-draw';
  *
+ * // Use the Material Icon pack.
+ * import { MaterialIconProvider } from '@js-draw/material-icons';
+ *
  * // Apply js-draw CSS
  * import 'js-draw/styles';
  * // If your bundler doesn't support the above, try
  * // import 'js-draw/bundledStyles';
  *
  * (async () => {
- *   const editor = new Editor(document.body);
+ *   const editor = new Editor(document.body, {
+ *     iconProvider: new MaterialIconProvider(),
+ *   });
  *   const toolbar = editor.addToolbar();
  *
  *   // Increases the minimum height of the editor
@@ -22,8 +27,8 @@
  *   await editor.loadFromSVG(`
  *       <svg viewBox="0 0 500 500" width="500" height="500" version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg">
  *           <style id="js-draw-style-sheet">path{stroke-linecap:round;stroke-linejoin:round;}text{white-space:pre;}</style>
- *           <path d="M500,500L500,0L0,0L0,500L500,500" fill="#423131bf" class="js-draw-image-background"></path>
- *           <text style="transform: matrix(1, 0, 0, 1, 57, 192); font-family: serif; font-size: 32px; fill: rgb(204, 102, 51);">Testing...</text>
+ *           <path d="M500,500L500,0L0,0L0,500L500,500" fill="#aaa" class="js-draw-image-background"></path>
+ *           <text style="transform: matrix(1, 0, 0, 1, 57, 192); font-family: serif; font-size: 32px; fill: #111;">Testing...</text>
  *       </svg>
  *   `);
  *
@@ -45,9 +50,10 @@
  * ```
  *
  * @see
- * {@link Editor}
- * {@link Editor.loadFromSVG}
- * {@link AbstractToolbar.addActionButton }
+ * - {@link Editor}
+ * - {@link Editor.loadFromSVG}
+ * - {@link AbstractToolbar.addActionButton }
+ * - {@link EditorSettings}
  *
  * @packageDocumentation
  */
