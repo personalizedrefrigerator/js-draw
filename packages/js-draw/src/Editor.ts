933e07e39ddeff264b7043613f149a65d3e02146
Henry Heino <personalizedrefrigerator@gmail.com>
2023-09-21 15:47:26 -0700

    Fix linter error

--- packages/js-draw/src/Editor.ts
+++ packages/js-draw/src/Editor.ts
@@ -8,7 +8,6 @@ import Viewport from './Viewport';
 import EventDispatcher from './EventDispatcher';
 import { Point2, Vec2, Vec3, Color4, Mat33, Rect2 } from '@js-draw/math';
 import Display, { RenderingMode } from './rendering/Display';
-import SVGRenderer from './rendering/renderers/SVGRenderer';
 import SVGLoader from './SVGLoader';
 import Pointer from './Pointer';
 import { EditorLocalization } from './localization';
