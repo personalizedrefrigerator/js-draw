2179d496e55675b73eb7d51c3839289a90c91f29
Henry Heino <personalizedrefrigerator@gmail.com>
2023-11-29 16:14:20 -0800

    chore: Refactor drag logic out of EdgeToolbar

--- packages/js-draw/src/toolbar/EdgeToolbar.ts
+++ packages/js-draw/src/toolbar/EdgeToolbar.ts
@@ -6,6 +6,7 @@ import EdgeToolbarLayoutManager from './widgets/layout/EdgeToolbarLayoutManager'
 import { MutableReactiveValue, ReactiveValue } from '../util/ReactiveValue';
 import AbstractToolbar, { SpacerOptions } from './AbstractToolbar';
 import stopPropagationOfScrollingWheelEvents from '../util/stopPropagationOfScrollingWheelEvents';
+import makeDraggable from './utils/makeDraggable';
 
 /**
  * Creates an `EdgeToolbar`.
@@ -61,6 +62,8 @@ export default class EdgeToolbar extends AbstractToolbar {
 	private sidebarVisible: MutableReactiveValue<boolean>;
 	private sidebarY: MutableReactiveValue<number>;
 	private sidebarTitle: MutableReactiveValue<string>;
+
+	private clearDragListeners: (()=>void)|null = null;
 
 	/** @internal */
 	public constructor(
@@ -341,18 +344,13 @@ export default class EdgeToolbar extends AbstractToolbar {
 		this.toolbarContainer.remove();
 		this.menuContainer.remove();
 		this.toolRowResizeObserver.disconnect();
+		this.clearDragListeners?.();
 	}
 
 	private initDragListeners() {
-		let lastX = 0;
-		let lastY = 0;
-		let startX = 0;
-		let startY = 0;
-		let pointerDown = false;
-		let capturedPointerId: number|null = null;
-
 		const dragElements = [ this.closeButton, this.sidebarContainer, this.sidebarContent ];
-
+		// Forward longer touch events from the menu background to the
+		// editor (and close the sidebar).
 		this.manageListener(this.editor.handlePointerEventsExceptClicksFrom(this.menuContainer, (eventName, event) => {
 			if (event.target === this.menuContainer) {
 				if (eventName === 'pointerdown') {
@@ -376,138 +374,29 @@ export default class EdgeToolbar extends AbstractToolbar {
 			return event.target === this.menuContainer;
 		}));
 
-		const isDraggableElement = (element: HTMLElement|null) => {
-			if (!element) {
-				return false;
-			}
-
-			if (dragElements.includes(element)) {
-				return true;
-			}
-
-			// Some inputs handle dragging themselves. Don't also interpret such gestures
-			// as dragging the dropdown.
-			const undraggableElementTypes = [ 'INPUT', 'SELECT', 'IMG' ];
-
-			let hasSuitableAncestors = false;
-			let ancestor = element.parentElement;
-			while (ancestor) {
-				if (undraggableElementTypes.includes(ancestor.tagName)) {
-					break;
-				}
-				if (ancestor === this.sidebarContainer) {
-					hasSuitableAncestors = true;
-					break;
-				}
-				ancestor = ancestor.parentElement;
-			}
-
-			return !undraggableElementTypes.includes(element.tagName) && hasSuitableAncestors;
-		};
-
-		const clickThreshold = 5;
-
-		// Returns whether the current (or if no current, **the last**) gesture is roughly a click.
-		// Because this can be called **after** a gesture has just ended, it should not require
-		// the gesture to be in progress.
-		const isRoughlyClick = () => {
-			return Math.hypot(lastX - startX, lastY - startY) < clickThreshold;
-		};
-
-		let startedDragging = false;
-
-		this.sidebarContainer.addEventListener('pointerdown', event => {
-			if (event.defaultPrevented || !isDraggableElement(event.target as HTMLElement)) {
-				return;
-			}
-
-			if (event.isPrimary) {
-				startedDragging = false;
-
-				lastX = event.clientX;
-				lastY = event.clientY;
-
-				startX = event.clientX;
-				startY = event.clientY;
-
-				capturedPointerId = null;
-				pointerDown = true;
-			}
-		}, { passive: true });
-
 		let gestureEndTimestamp = 0;
-		const onGestureEnd = (_event: Event) => {
-			// If the pointerup/pointercancel event was for a pointer not being tracked,
-			if (!pointerDown) {
-				return;
-			}
-
-			gestureEndTimestamp = performance.now();
-
-			if (capturedPointerId !== null) {
-				this.sidebarContainer.releasePointerCapture(capturedPointerId);
-				capturedPointerId = null;
-			}
-
-			this.finalizeDrag();
-			pointerDown = false;
-			startedDragging = false;
-		};
-
-		this.sidebarContainer.onpointermove = event => {
-			if (!event.isPrimary || !pointerDown) {
-				return undefined;
-			}
-
-			// Mouse event and no buttons pressed? Cancel the event.
-			// This can happen if the event was canceled by a focus change (e.g. by opening a
-			// right-click menu).
-			if (event.pointerType === 'mouse' && event.buttons === 0) {
-				onGestureEnd(event);
-				return undefined;
-			}
-
-			// Only capture after motion -- capturing early prevents click events in Chrome.
-			if (capturedPointerId === null && !isRoughlyClick()) {
-				this.sidebarContainer.setPointerCapture(event.pointerId);
-				capturedPointerId = event.pointerId;
-			}
-
-			const x = event.clientX;
-			const y = event.clientY;
-			const dx = x - lastX;
-			const dy = y - lastY;
-
-			if (Math.abs(y - startY) > clickThreshold || startedDragging) {
-				this.handleDrag(dx, dy);
-
-				lastX = x;
-				lastY = y;
-
-				startedDragging = true;
-			}
-		};
-
-		this.sidebarContainer.onpointerleave = event => {{
-			// Capture the pointer if it exits the container while dragging.
-			if (capturedPointerId === null && pointerDown && event.isPrimary) {
-				this.sidebarContainer.setPointerCapture(event.pointerId);
-				capturedPointerId = event.pointerId;
-			}
-		}};
+		let lastGestureWasRoughlyClick = false;
+		const dragController = makeDraggable(this.sidebarContainer, {
+			draggableChildElements: dragElements,
+			onDrag: (deltaX, deltaY) => this.handleDrag(deltaX, deltaY),
+			onDragEnd: (dragStatistics) => {
+				gestureEndTimestamp = dragStatistics.endTimestamp;
+				lastGestureWasRoughlyClick = dragStatistics.roughlyClick;
+				this.finalizeDrag();
+			},
+		});
+		this.clearDragListeners = () => dragController.removeListeners();
 
 		this.closeButton.onclick = () => {
 			const wasJustDragging = performance.now() - gestureEndTimestamp < 100;
-			const roughlyClick = isRoughlyClick();
 
 			// Ignore the click event if it was caused by dragging the button.
-			if (wasJustDragging && roughlyClick || !wasJustDragging) {
+			if (
+				(wasJustDragging && lastGestureWasRoughlyClick) || !wasJustDragging
+			) {
 				this.sidebarVisible.set(false);
 			}
 		};
-
-		this.sidebarContainer.onpointerup = onGestureEnd;
-		this.sidebarContainer.onpointercancel = onGestureEnd;
 	}
 
 	/**
