personalizedrefrigerator/js-draw

commit 9f941487dc040f5628007cafd77cd060b79b4aed

feat: Copy a PNG image of the drawing in addition to SVG data in suported browsers (#60)
This commit uses the `Clipboard` API to copy PNG data in addition to the SVG data as HTML in browsers that support the Clipboard API. Currently, this is Chrome and Safari.

packages/js-draw/src/tools/PasteHandler.ts
@@ -29,7 +29,7 @@ export default class PasteHandler extends BaseTool {
 	public override onPaste(event: PasteEvent): boolean {
 		const mime = event.mime.toLowerCase();
 
-		if (mime === 'image/svg+xml') {
+		if (mime === 'image/svg+xml' || mime === 'text/html') {
 			void this.doSVGPaste(event.data);
 			return true;
 		}
@@ -50,17 +50,20 @@ export default class PasteHandler extends BaseTool {
 	}
 
 	private async doSVGPaste(data: string) {
-		const sanitize = true;
-		const loader = SVGLoader.fromString(data, sanitize);
+		this.editor.showLoadingWarning(0);
+		try {
+			const loader = SVGLoader.fromString(data, true);
 
-		const components: AbstractComponent[] = [];
+			const components: AbstractComponent[] = [];
+			await loader.start((component) => {
+				components.push(component);
+			},
+			(_countProcessed: number, _totalToProcess: number) => null);
 
-		await loader.start((component) => {
-			components.push(component);
-		},
-		(_countProcessed: number, _totalToProcess: number) => null);
-
-		await this.addComponentsFromPaste(components);
+			await this.addComponentsFromPaste(components);
+		} finally {
+			this.editor.hideLoadingWarning();
+		}
 	}
 
 	private async doTextPaste(text: string) {

packages/js-draw/src/Editor.ts
@@ -15,7 +15,6 @@ import getLocalizationTable from './localizations/getLocalizationTable';
 import IconProvider from './toolbar/IconProvider';
 import CanvasRenderer from './rendering/renderers/CanvasRenderer';
 import untilNextAnimationFrame from './util/untilNextAnimationFrame';
-import fileToBase64Url from './util/fileToBase64Url';
 import uniteCommands from './commands/uniteCommands';
 import SelectionTool from './tools/SelectionTool/SelectionTool';
 import AbstractComponent from './components/AbstractComponent';
@@ -36,6 +35,7 @@ import ReactiveValue, { MutableReactiveValue } from './util/ReactiveValue';
 import listenForKeyboardEventsFrom from './util/listenForKeyboardEventsFrom';
 import mitLicenseAttribution from './util/mitLicenseAttribution';
 import { PenTypeRecord } from './toolbar/widgets/PenToolWidget';
+import ClipboardHandler from './util/ClipboardHandler';
 
 /**
  * Provides settings to an instance of an editor. See the Editor {@link Editor.constructor}.
@@ -542,21 +542,14 @@ export class Editor {
 			this.accessibilityControlArea.value = '';
 		});
 
-		document.addEventListener('copy', evt => {
+		const copyHandler = new ClipboardHandler(this);
+
+		document.addEventListener('copy', async evt => {
 			if (!this.isEventSink(document.querySelector(':focus'))) {
 				return;
 			}
 
-			const clipboardData = evt.clipboardData;
-
-			if (this.toolController.dispatchInputEvent({
-				kind: InputEvtType.CopyEvent,
-				setData: (mime, data) => {
-					clipboardData?.setData(mime, data);
-				},
-			})) {
-				evt.preventDefault();
-			}
+			copyHandler.copy(evt);
 		});
 
 		document.addEventListener('paste', evt => {
@@ -757,72 +750,7 @@ export class Editor {
 			return;
 		}
 
-		const clipboardData: DataTransfer = (evt as any).dataTransfer ?? (evt as any).clipboardData;
-		if (!clipboardData) {
-			return;
-		}
-
-		// Handle SVG files (prefer to PNG/JPEG)
-		for (const file of clipboardData.files) {
-			if (file.type.toLowerCase() === 'image/svg+xml') {
-				const text = await file.text();
-				if (this.toolController.dispatchInputEvent({
-					kind: InputEvtType.PasteEvent,
-					mime: file.type,
-					data: text,
-				})) {
-					evt.preventDefault();
-					return;
-				}
-			}
-		}
-
-		// Handle image files.
-		for (const file of clipboardData.files) {
-			const fileType = file.type.toLowerCase();
-			if (fileType === 'image/png' || fileType === 'image/jpg') {
-				this.showLoadingWarning(0);
-				const onprogress = (evt: ProgressEvent<FileReader>) => {
-					this.showLoadingWarning(evt.loaded / evt.total);
-				};
-
-				try {
-					const data = await fileToBase64Url(file, { onprogress });
-
-					if (data && this.toolController.dispatchInputEvent({
-						kind: InputEvtType.PasteEvent,
-						mime: fileType,
-						data: data,
-					})) {
-						evt.preventDefault();
-						this.hideLoadingWarning();
-						return;
-					}
-				} catch (e) {
-					console.error('Error reading image:', e);
-				}
-				this.hideLoadingWarning();
-			}
-		}
-
-		// Supported MIMEs for text data, in order of preference
-		const supportedMIMEs = [
-			'image/svg+xml',
-			'text/plain',
-		];
-
-		for (const mime of supportedMIMEs) {
-			const data = clipboardData.getData(mime);
-
-			if (data && this.toolController.dispatchInputEvent({
-				kind: InputEvtType.PasteEvent,
-				mime,
-				data,
-			})) {
-				evt.preventDefault();
-				return;
-			}
-		}
+		return await new ClipboardHandler(this).paste(evt);
 	}
 
 	/**
@@ -1445,22 +1373,7 @@ export class Editor {
 	 * [[include:doc-pages/inline-examples/adding-an-image-and-data-urls.md]]
 	 */
 	public toDataURL(format: 'image/png'|'image/jpeg'|'image/webp' = 'image/png', outputSize?: Vec2): string {
-		const canvas = document.createElement('canvas');
-
-		const importExportViewport = this.image.getImportExportViewport();
-		const exportRectSize = importExportViewport.getScreenRectSize();
-		const resolution = outputSize ?? exportRectSize;
-
-		canvas.width = resolution.x;
-		canvas.height = resolution.y;
-
-		const ctx = canvas.getContext('2d')!;
-
-		// Scale to ensure that the entire output is visible.
-		const scaleFactor = Math.min(resolution.x / exportRectSize.x, resolution.y / exportRectSize.y);
-		ctx.scale(scaleFactor, scaleFactor);
-
-		const renderer = new CanvasRenderer(ctx, importExportViewport);
+		const { element: canvas, renderer } = CanvasRenderer.fromViewport(this.image.getImportExportViewport(), { canvasSize: outputSize });
 
 		this.image.renderAll(renderer);
 

packages/js-draw/src/inputEvents.ts
@@ -80,7 +80,7 @@ export interface KeyUpEvent extends BaseKeyEvent {
 
 export interface CopyEvent {
 	readonly kind: InputEvtType.CopyEvent;
-	setData(mime: string, data: string): void;
+	setData(mime: string, data: string|Promise<Blob>): void;
 }
 
 export interface PasteEvent {

packages/js-draw/src/rendering/renderers/CanvasRenderer.ts
@@ -310,4 +310,26 @@ export default class CanvasRenderer extends AbstractRenderer {
 
 		return bothTooSmall || anyTooSmall;
 	}
+
+	// @internal
+	public static fromViewport(exportViewport: Viewport, options: { canvasSize?: Vec2, maxCanvasDimen?: number } = {}) {
+		const canvas = document.createElement('canvas');
+
+		const exportRectSize = exportViewport.getScreenRectSize();
+		let canvasSize = options.canvasSize ?? exportRectSize;
+		if (options.maxCanvasDimen && canvasSize.maximumEntryMagnitude() > options.maxCanvasDimen) {
+			canvasSize = canvasSize.times(options.maxCanvasDimen / canvasSize.maximumEntryMagnitude());
+		}
+
+		canvas.width = canvasSize.x;
+		canvas.height = canvasSize.y;
+
+		const ctx = canvas.getContext('2d')!;
+
+		// Scale to ensure that the entire output is visible.
+		const scaleFactor = Math.min(canvasSize.x / exportRectSize.x, canvasSize.y / exportRectSize.y);
+		ctx.scale(scaleFactor, scaleFactor);
+
+		return { renderer: new CanvasRenderer(ctx, exportViewport), element: canvas };
+	}
 }

packages/js-draw/src/tools/SelectionTool/SelectionTool.ts
@@ -5,6 +5,7 @@ import { EditorEventType } from '../../types';
 import { CopyEvent, KeyPressEvent, KeyUpEvent, PointerEvt } from '../../inputEvents';
 import Viewport from '../../Viewport';
 import BaseTool from '../BaseTool';
+import CanvasRenderer from '../../rendering/renderers/CanvasRenderer';
 import SVGRenderer from '../../rendering/renderers/SVGRenderer';
 import Selection from './Selection';
 import TextComponent from '../../components/TextComponent';
@@ -483,10 +484,18 @@ export default class SelectionTool extends BaseTool {
 
 		const sanitize = true;
 		const { element: svgExportElem, renderer: svgRenderer } = SVGRenderer.fromViewport(exportViewport, sanitize);
+		const { element: canvas, renderer: canvasRenderer } = CanvasRenderer.fromViewport(
+			exportViewport,
+			{
+				canvasSize: this.selectionBox.getScreenRegion().size,
+				maxCanvasDimen: 2048,
+			},
+		);
 
 		const text: string[] = [];
 		for (const elem of selectedElems) {
 			elem.render(svgRenderer);
+			elem.render(canvasRenderer);
 
 			if (elem instanceof TextComponent) {
 				text.push(elem.getText());
@@ -495,6 +504,15 @@ export default class SelectionTool extends BaseTool {
 
 		event.setData('image/svg+xml', svgExportElem.outerHTML);
 		event.setData('text/html', svgExportElem.outerHTML);
+		event.setData('image/png', new Promise<Blob>((resolve, reject) => {
+			canvas.toBlob((blob) => {
+				if (blob) {
+					resolve(blob);
+				} else {
+					reject('Failed to convert canvas to blob.');
+				}
+			}, 'image/png');
+		}));
 		if (text.length > 0) {
 			event.setData('text/plain', text.join('\n'));
 		}

packages/js-draw/src/tools/ToolController.ts
@@ -145,14 +145,22 @@ export default class ToolController implements InputEventListener {
 		});
 	}
 
-	// Add a tool to the end of this' tool list (the added tool receives events after tools already added to this).
-	// This should be called before creating the app's toolbar.
-	//
-	// A tool should only be added once.
-	public addTool(tool: BaseTool) {
+	/**
+	 * Add a tool to the end of this' tool list (the added tool receives events after tools already added to this).
+	 * This should be called before creating the app's toolbar.
+	 *
+	 * If `options.addToFront`, the tool is added to the beginning of this' tool list.
+	 *
+	 * Does nothing if the tool is already present.
+	 */
+	public addTool(tool: BaseTool, options?: { addToFront: boolean }) {
 		// Only add if not already present.
 		if (!this.tools.includes(tool)) {
-			this.tools.push(tool);
+			if (options?.addToFront) {
+				this.tools.splice(0, 0, tool);
+			} else {
+				this.tools.push(tool);
+			}
 		}
 	}
 

packages/js-draw/src/util/ClipboardHandler.test.ts
@@ -1,0 +1,172 @@
+import BaseTool from '../tools/BaseTool';
+import createEditor from '../testing/createEditor';
+import ClipboardHandler from './ClipboardHandler';
+import { CopyEvent, PasteEvent } from '../inputEvents';
+import Editor from '../Editor';
+
+type ClipboardTestData = Record<string, string|Blob>;
+
+// A tool that handles all copy events
+class TestCopyPasteTool extends BaseTool {
+	public constructor(editor: Editor, private data: ClipboardTestData) {
+		super(editor.notifier, 'copy tool');
+	}
+
+	public override onCopy(event: CopyEvent): boolean {
+		for (const key in this.data) {
+			const data = this.data[key];
+			if (typeof data === 'string') {
+				event.setData(key, data);
+			} else {
+				event.setData(key, Promise.resolve(data));
+			}
+		}
+
+		return true;
+	}
+
+	public lastPasteData: PasteEvent;
+	public override onPaste(event: PasteEvent): boolean {
+		this.lastPasteData = { ...event };
+		return false;
+	}
+}
+
+const setUpCopyPasteTool = (editor: Editor, dataToCopy: ClipboardTestData) => {
+	const copyTool = new TestCopyPasteTool(editor, dataToCopy);
+	editor.toolController.addTool(copyTool, { addToFront: true });
+	return copyTool;
+};
+
+const originalClipboardItem = window.ClipboardItem;
+const setClipboardApiSupported = (supported: boolean) => {
+	if (supported) {
+		window.ClipboardItem = originalClipboardItem;
+	} else {
+		window.ClipboardItem = undefined as any;
+	}
+};
+
+describe('ClipboardHandler', () => {
+	beforeEach(async () => {
+		setClipboardApiSupported(true);
+		await navigator.clipboard.write([]);
+	});
+
+	describe('copy', () => {
+		it('should copy to the clipboard API when no event is given', async () => {
+			const editor = createEditor();
+			setUpCopyPasteTool(editor, {
+				'text/plain': 'Test.',
+			});
+
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			await clipboardHandler.copy();
+
+			expect(await navigator.clipboard.readText()).toBe('Test.');
+		});
+
+		it('should copy to the clipboard API when images are to be copied', async () => {
+			const editor = createEditor();
+			setUpCopyPasteTool(editor, {
+				'text/plain': 'Testing!',
+				'text/html': '<i>Testing!</i>',
+				'image/png': 'Fake image.',
+			});
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			const event = new ClipboardEvent('copy', { clipboardData: new DataTransfer() });
+			await clipboardHandler.copy(event);
+
+			// Should not have written to the clipboard event
+			expect(event.clipboardData?.getData('text/plain')).toBe('');
+			expect(event.clipboardData?.getData('text/html')).toBe('');
+
+			// Should have written to navigator.clipboard
+			const clipboardItems = await navigator.clipboard.read();
+			const itemTypes = clipboardItems.map(item => item.types).flat();
+			expect(itemTypes).toContain('image/png');
+			expect(itemTypes).toContain('text/plain');
+			expect(itemTypes).toContain('text/html');
+		});
+
+		it.each([
+			[{
+				'text/plain': 'Testing!',
+				'text/html': '<i>Testing!</i>',
+			}],
+			[{
+				'text/plain': 'Test 2',
+				'text/html': '<i>...</i>',
+
+				// image/svg+xml is a text format, so shouldn't cause navigator.clipboard to be used
+				'image/svg+xml': 'test',
+			}]
+		])('should use event.dataTransfer when no images are to be copied (case %#)', async (data) => {
+			const editor = createEditor();
+			setUpCopyPasteTool(editor, data);
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			const event = new ClipboardEvent('copy', { clipboardData: new DataTransfer() });
+			await clipboardHandler.copy(event);
+
+			expect(event.clipboardData?.getData('text/plain')).toBe(data['text/plain']);
+			expect(event.clipboardData?.getData('text/html')).toBe(data['text/html']);
+		});
+
+		it('should use event.dataTransfer when the clipboard API is unsupported', async () => {
+			setClipboardApiSupported(false);
+
+			const editor = createEditor();
+			setUpCopyPasteTool(editor, {
+				'text/plain': 'This should be copied.',
+				'image/png': 'Fake image.',
+			});
+
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			const event = new ClipboardEvent('copy', { clipboardData: new DataTransfer() });
+			await clipboardHandler.copy(event);
+
+			expect(event.clipboardData?.getData('text/plain')).toBe('This should be copied.');
+			expect(await navigator.clipboard.read()).toHaveLength(0);
+		});
+
+		// image/svg+xml is unsupported in Chrome as of early 2024.
+		it('should copy text/html instead of image/svg+xml when copying to the Clipboard API', async () => {
+			const editor = createEditor();
+			setUpCopyPasteTool(editor, {
+				'image/svg+xml': '<svg>This should be copied.</svg>',
+			});
+
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			await clipboardHandler.copy();
+
+			const clipboardItems = await navigator.clipboard.read();
+			expect(clipboardItems).toHaveLength(1);
+			expect(await (await clipboardItems[0].getType('text/html')).text()).toBe('<svg>This should be copied.</svg>');
+		});
+	});
+
+	describe('paste', () => {
+		it('should support pasting from the clipboard API', async () => {
+			const editor = createEditor();
+			const clipboardHandler = new ClipboardHandler(editor);
+
+			const testData = '<svg><text>Testing...</text></svg>';
+			const copyPasteTool = setUpCopyPasteTool(editor, {
+				'image/svg+xml': testData,
+			});
+			await clipboardHandler.copy();
+			await clipboardHandler.paste();
+
+			// Should have pasted
+			expect(copyPasteTool.lastPasteData).toMatchObject({
+				mime: 'text/html',
+				data: testData,
+			});
+		});
+	});
+});

packages/js-draw/src/util/ClipboardHandler.ts
@@ -1,0 +1,214 @@
+
+import { Editor } from '../Editor';
+import { InputEvtType } from '../inputEvents';
+import fileToBase64Url from './fileToBase64Url';
+
+const isTextMimeType = (mime: string) =>
+	// +xml: Handles image/svg+xml
+	mime.endsWith('+xml') || mime.startsWith('text/');
+
+/**
+ * Handles conversion between the browser clipboard APIs and internal
+ * js-draw clipboard events.
+ */
+export default class ClipboardHandler {
+	#preferClipboardEvents = false;
+
+	public constructor(private editor: Editor) {
+	}
+
+	/**
+	 * Pastes data from the clipboard into the editor associated with
+	 * this handler.
+	 *
+	 * @param event Optional -- a clipboard/drag event. If not provided,
+	 * 				`navigator.clipboard` will be used instead.
+	 * @returns true if the paste event was handled by the editor.
+	 */
+	public async paste(event?: DragEvent|ClipboardEvent) {
+		const editor = this.editor;
+
+		const clipboardData: DataTransfer|null = (event as any)?.dataTransfer ?? (event as any)?.clipboardData ?? null;
+		const hasEvent = !!clipboardData;
+
+		const sendPasteEvent = (mime: string, data: string|null) => {
+			return data && editor.toolController.dispatchInputEvent({
+				kind: InputEvtType.PasteEvent,
+				mime,
+				data,
+			});
+		};
+
+		// Listed in order of precedence
+		const supportedMIMEs = [
+			'image/svg+xml',
+			'text/html',
+			'image/png',
+			'image/jpeg',
+			'text/plain',
+		];
+
+		let files: Blob[] = [];
+		const textData: Map<string, string> = new Map<string, string>();
+		if (hasEvent) {
+			// NOTE: On some browsers, .getData and .files must be used before any async operations.
+			files = [...clipboardData.files];
+			for (const mime of supportedMIMEs) {
+				const data = clipboardData.getData(mime);
+				if (data) {
+					textData.set(mime, data);
+				}
+			}
+		} else {
+			const clipboardData = await navigator.clipboard.read();
+			for (const item of clipboardData) {
+				for (const mime of item.types) {
+					if (supportedMIMEs.includes(mime)) {
+						files.push(await item.getType(mime));
+					}
+				}
+			}
+		}
+
+		// Returns true if handled
+		const handleMIME = async (mime: string) => {
+			const isTextFormat = isTextMimeType(mime);
+			if (isTextFormat) {
+				const data = textData.get(mime)!;
+
+				if (sendPasteEvent(mime, data)) {
+					event?.preventDefault();
+					return true;
+				}
+			}
+
+			for (const file of files) {
+				const fileType = file.type.toLowerCase();
+				if (fileType !== mime) {
+					continue;
+				}
+
+				if (isTextFormat) {
+					const text = await file.text();
+					if (sendPasteEvent(mime, text)) {
+						event?.preventDefault();
+						return true;
+					}
+				} else {
+					editor.showLoadingWarning(0);
+					const onprogress = (evt: ProgressEvent<FileReader>) => {
+						editor.showLoadingWarning(evt.loaded / evt.total);
+					};
+
+					try {
+						const data = await fileToBase64Url(file, { onprogress });
+
+						if (sendPasteEvent(mime, data)) {
+							event?.preventDefault();
+							editor.hideLoadingWarning();
+							return true;
+						}
+					} catch (e) {
+						console.error('Error reading image:', e);
+					}
+					editor.hideLoadingWarning();
+				}
+			}
+			return false;
+		};
+
+		for (const mime of supportedMIMEs) {
+			if (await handleMIME(mime)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Copies text from the editor associated with this.
+	 *
+	 * Even if `event` is provided, the `navigator.clipboard` API may be used if image data
+	 * is to be copied. This is done because `ClipboardEvent`s seem to not support attaching
+	 * images.
+	 */
+	public copy(event?: ClipboardEvent) {
+		const mimeToData: Record<string, Promise<Blob>|string> = Object.create(null);
+
+		if (this.editor.toolController.dispatchInputEvent({
+			kind: InputEvtType.CopyEvent,
+			setData: (mime, data) => {
+				mimeToData[mime] = data;
+			},
+		})) {
+			event?.preventDefault();
+		}
+
+		const mimeTypes = Object.keys(mimeToData);
+		const hasNonTextMimeTypes = mimeTypes.some(mime => !isTextMimeType(mime));
+
+		const copyToEvent = () => {
+			if (!event) {
+				throw new Error('Unable to paste -- no event provided.');
+			}
+
+			for (const key in mimeToData) {
+				const value = mimeToData[key];
+				if (typeof value === 'string') {
+					event.clipboardData?.setData(key, value);
+				}
+			}
+		};
+
+		const copyToClipboardApi = () => {
+			const mappedMimeToData: Record<string, Blob|Promise<Blob>> = Object.create(null);
+			const mimeMapping: Record<string, string> = {
+				// image/svg+xml is unsupported in Chrome.
+				'image/svg+xml': 'text/html',
+			};
+			for (const key in mimeToData) {
+				const data = mimeToData[key];
+				const mappedKey = mimeMapping[key] || key;
+				if (typeof data === 'string') {
+					mappedMimeToData[mappedKey] = new Blob([new TextEncoder().encode(data)], { type: mappedKey });
+				} else {
+					mappedMimeToData[mappedKey] = data;
+				}
+			}
+			return navigator.clipboard.write([ new ClipboardItem(mappedMimeToData) ]);
+		};
+
+		const supportsClipboardApi = (
+			typeof ClipboardItem !== 'undefined'
+			&& typeof navigator?.clipboard?.write !== 'undefined'
+		);
+		if (!this.#preferClipboardEvents && supportsClipboardApi && (hasNonTextMimeTypes || !event)) {
+			let clipboardApiPromise: Promise<void>|null = null;
+
+			const fallBackToCopyEvent = (reason: any) => {
+				console.warn(
+					'Unable to copy to the clipboard API. Future calls to .copy will use ClipboardEvents if possible.',
+					reason
+				);
+				this.#preferClipboardEvents = true;
+
+				copyToEvent();
+			};
+
+			try {
+				clipboardApiPromise = copyToClipboardApi();
+			} catch(error) {
+				fallBackToCopyEvent(error);
+			}
+
+			if (clipboardApiPromise) {
+				return clipboardApiPromise.catch(fallBackToCopyEvent);
+			}
+		} else {
+			copyToEvent();
+		}
+
+		return Promise.resolve();
+	}
+}

commit f2812ee196bd8d946a6d6fef119b9af3368c9c5d

fix: Fix pasting text that also has HTML formatting
Regression from 9f941487dc040f5628007cafd77cd060b79b4aed

packages/js-draw/src/tools/PasteHandler.ts
@@ -29,7 +29,21 @@ export default class PasteHandler extends BaseTool {
 	public override onPaste(event: PasteEvent): boolean {
 		const mime = event.mime.toLowerCase();
 
-		if (mime === 'image/svg+xml' || mime === 'text/html') {
+		const isSvgData = (() => {
+			if (mime === 'image/svg+xml') {
+				return true;
+			}
+
+			if (mime !== 'text/html') {
+				return false;
+			}
+
+			// text/html is sometimes handlable SVG data. Use a hueristic
+			// to determine if this is the case:
+			return event.data.match(/^\s*<svg/i);
+		})();
+
+		if (isSvgData) {
 			void this.doSVGPaste(event.data);
 			return true;
 		}