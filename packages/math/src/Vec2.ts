3e9708794a7e13b62674f928dcb1946e69f7dff9
Henry Heino <personalizedrefrigerator@gmail.com>
2024-04-12 19:58:37 -0700

    Improve Vec2 commenting

--- packages/math/src/Vec2.ts
+++ packages/math/src/Vec2.ts
@@ -1,3 +1,6 @@
+// Internally, we define Vec2 as a namespace within Vec3 --
+// this allows referencing Vec2s from Vec3 constructors without
+// cyclic references.
 import { Vec3, Vec2 } from './Vec3';
 
 export type Point2 = Vec3;
