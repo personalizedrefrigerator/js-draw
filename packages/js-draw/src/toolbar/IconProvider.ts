personalizedrefrigerator/js-draw

commit 0970ab8bed05c4996e388b0730619f5053ea552f

chore: Add the eslint-no-unsanitized rule (#85)
This commit adds the eslint-plugin-no-unsanitized rule, which flags most usages of .innerHTML and document.write as unsafe.

packages/js-draw/src/toolbar/IconProvider.ts
@@ -7,16 +7,11 @@ import Viewport from '../Viewport';
 import { makeFreehandLineBuilder } from '../components/builders/FreehandLineBuilder';
 import { makePolylineBuilder } from '../components/builders/PolylineBuilder';
 import { EraserMode } from '../tools/Eraser';
+import { createSvgElement, createSvgElements, createSvgPaths } from '../util/createElement';
 
 export type IconElemType = HTMLImageElement | SVGElement;
 
 const svgNamespace = 'http://www.w3.org/2000/svg';
-const iconColorFill = `
-	style='fill: var(--icon-color);'
-`;
-const iconColorStrokeFill = `
-	style='fill: var(--icon-color); stroke: var(--icon-color);'
-`;
 
 let checkerboardIdCounter = 0;
 const makeCheckerboardPattern = () => {
@@ -36,7 +31,15 @@ const makeCheckerboardPattern = () => {
 	`;
 	const patternRef = `url(#${id})`;
 
-	return { patternDef, patternRef };
+	return {
+		patternDefElement: (): SVGElement => {
+			const element = new DOMParser().parseFromString(patternDef, 'image/svg+xml');
+			return element.firstChild as SVGElement;
+		},
+		// @deprecated use patternDefElement
+		patternDef,
+		patternRef,
+	};
 };
 
 const makeRedoIcon = (mirror: boolean) => {
@@ -53,11 +56,16 @@ const makeRedoIcon = (mirror: boolean) => {
 				transform-origin: center;
 			}
 		</style>
-		<path
-			d='M20,20 A15,15 0 0 1 70,80 L80,90 L60,70 L65,90 L87,90 L65,80'
-			class='toolbar-svg-undo-redo-icon'
-			style='${mirror ? 'transform: scale(-1, 1);' : ''}'/>
 	`;
+
+	const path = document.createElementNS(svgNamespace, 'path');
+	path.setAttribute('d', 'M20,20 A15,15 0 0 1 70,80 L80,90 L60,70 L65,90 L87,90 L65,80');
+	path.classList.add('toolbar-svg-undo-redo-icon');
+	if (mirror) {
+		path.style.transform = 'scale(-1, 1)';
+	}
+	icon.appendChild(path);
+
 	icon.setAttribute('viewBox', '0 0 100 100');
 	return icon;
 };
@@ -105,68 +113,65 @@ export default class IconProvider {
 	}
 
 	public makeDropdownIcon(): IconElemType {
-		const icon = document.createElementNS(svgNamespace, 'svg');
-		icon.innerHTML = `
-		<g>
-			<path
-				d='M5,10 L50,90 L95,10 Z'
-				${iconColorFill}
-			/>
-		</g>
-		`;
+		const icon = this.makeIconFromPath('M5,10 L50,90 L95,10 Z');
 		icon.setAttribute('viewBox', '-10 -10 110 110');
 		return icon;
 	}
 
 	public makeEraserIcon(eraserSize?: number, mode?: EraserMode): IconElemType {
-		const icon = document.createElementNS(svgNamespace, 'svg');
 		eraserSize ??= 10;
 
 		const scaledSize = eraserSize / 4;
 		const eraserColor = '#ff70af';
 
 		// Draw an eraser-like shape. Created with Inkscape
-		icon.innerHTML = `
-		<defs>
-			<linearGradient id="dash-pattern">
-				<stop offset="80%" stop-color="${eraserColor}"/>
-				<stop offset="85%" stop-color="white"/>
-				<stop offset="90%" stop-color="${eraserColor}"/>
-			</linearGradient>
-		</defs>
-		<g>
-			<path
-				style="fill:${mode === EraserMode.PartialStroke ? 'url(#dash-pattern)' : eraserColor}"
-				stroke="black"
-				transform="rotate(41.35)"
-				d="M 52.5 27
-					C 50 28.9 48.9 31.7 48.9 34.8
-					L 48.9 39.8
-					C 48.9 45.3 53.4 49.8 58.9 49.8
-					L 103.9 49.8
-					C 105.8 49.8 107.6 49.2 109.1 48.3
-					L 110.2 ${scaledSize + 49.5} L 159.7 ${scaledSize + 5}
-					L 157.7 ${-scaledSize + 5.2} L 112.4 ${49.5 - scaledSize}
-					C 113.4 43.5 113.9 41.7 113.9 39.8
-					L 113.9 34.8
-					C 113.9 29.3 109.4 24.8 103.9 24.8
-					L 58.9 24.8
-					C 56.5 24.8 54.3 25.7 52.5 27
-					z "
-				id="path438" />
-
-			<rect
-				stroke="#cc8077"
-				${iconColorFill}
-				id="rect218"
-				width="65"
-				height="75"
-				x="48.9"
-				y="-38.7"
-				transform="rotate(41.35)" />
-		</g>
-		`;
-		icon.setAttribute('viewBox', '0 0 120 120');
+		const icon = createSvgElement('svg', {
+			viewBox: '0 0 120 120',
+			children: [
+				createSvgElement('defs', {
+					children: [
+						createSvgElement('linearGradient', {
+							id: 'dash-pattern',
+							children: createSvgElements('stop', [
+								{ offset: '80%', 'stop-color': eraserColor },
+								{ offset: '85%', 'stop-color': 'white' },
+								{ offset: '90%', 'stop-color': eraserColor },
+							]),
+						}),
+					],
+				}),
+				createSvgElement('path', {
+					fill: mode === EraserMode.PartialStroke ? 'url(#dash-pattern)' : eraserColor,
+					stroke: 'black',
+					transform: 'rotate(41.35)',
+					d: `
+						M 52.5 27
+						C 50 28.9 48.9 31.7 48.9 34.8
+						L 48.9 39.8
+						C 48.9 45.3 53.4 49.8 58.9 49.8
+						L 103.9 49.8
+						C 105.8 49.8 107.6 49.2 109.1 48.3
+						L 110.2 ${scaledSize + 49.5} L 159.7 ${scaledSize + 5}
+						L 157.7 ${-scaledSize + 5.2} L 112.4 ${49.5 - scaledSize}
+						C 113.4 43.5 113.9 41.7 113.9 39.8
+						L 113.9 34.8
+						C 113.9 29.3 109.4 24.8 103.9 24.8
+						L 58.9 24.8
+						C 56.5 24.8 54.3 25.7 52.5 27
+						z
+					`,
+				}),
+				createSvgElement('rect', {
+					stroke: '#cc8077',
+					fill: 'var(--icon-color)',
+					width: 65,
+					height: 75,
+					x: 48.9,
+					y: -38.7,
+					transform: 'rotate(41.35)',
+				}),
+			],
+		});
 		return icon;
 	}
 
@@ -176,8 +181,8 @@ export default class IconProvider {
 		// Draw a cursor-like shape
 		icon.innerHTML = `
 		<g>
-			<rect x=10 y=10 width=70 height=70 fill='pink' stroke='black'/>
-			<rect x=75 y=75 width=10 height=10 fill='white' stroke='black'/>
+			<rect x="10" y="10" width="70" height="70" fill="pink" stroke="black"/>
+			<rect x="75" y="75" width="10" height="10" fill="white" stroke="black"/>
 		</g>
 		`;
 		icon.setAttribute('viewBox', '0 0 100 100');
@@ -483,8 +488,6 @@ export default class IconProvider {
 		const color = penStyle.color;
 		const rounded = this.isRoundedTipPen(penStyle);
 
-		const icon = document.createElementNS(svgNamespace, 'svg');
-		icon.setAttribute('viewBox', '0 0 100 100');
 		const tipThickness = strokeSize / 2;
 
 		const inkTipPath = `
@@ -525,74 +528,54 @@ export default class IconProvider {
 
 		const checkerboardPattern = makeCheckerboardPattern();
 
-		const ink = `
-			<path
-				fill="${checkerboardPattern.patternRef}"
-				d="${inkTipPath}"
-			/>
-			<path
-				fill="${checkerboardPattern.patternRef}"
-				d="${inkTrailPath}"
-			/>
-			<path
-				fill="${color}"
-				d="${inkTipPath}"
-			/>
-			<path
-				fill="${color}"
-				d="${inkTrailPath}"
-			/>
-		`;
+		const colorString = color.toHexString();
+		const ink = createSvgPaths(
+			{
+				fill: checkerboardPattern.patternRef,
+				d: inkTipPath,
+			},
+			{
+				fill: checkerboardPattern.patternRef,
+				d: inkTrailPath,
+			},
+			{
+				fill: colorString,
+				d: inkTipPath,
+			},
+			{
+				fill: colorString,
+				d: inkTrailPath,
+			},
+		);
 
-		const penTip = `
-			<path
-				fill="${checkerboardPattern.patternRef}"
-				d="${penTipPath}"
-			/>
-			<path
-				fill="${tipColor}"
-				stroke="${color}"
-				d="${penTipPath}"
-			/>
-		`;
+		const penTip = createSvgPaths(
+			{ fill: checkerboardPattern.patternRef, d: penTipPath },
+			{ fill: tipColor, stroke: colorString, d: penTipPath },
+		);
 
-		const grip = `
-			<path
-				${iconColorStrokeFill}
-				d="${gripMainPath}"
-			/>
+		const grip = createSvgPaths(
+			{ fill: 'var(--icon-color)', stroke: 'var(--icon-color)', d: gripMainPath },
 
-			<!-- shadows -->
-			<path
-				fill="rgba(150, 150, 150, 0.3)"
-				d="${gripShadow1Path}"
-			/>
-			<path
-				fill="rgba(100, 100, 100, 0.2)"
-				d="${gripShadow2Path}"
-			/>
+			// Shadows
+			{ fill: 'rgba(150, 150, 150, 0.3)', d: gripShadow1Path },
+			{ fill: 'rgba(100, 100, 100, 0.2)', d: gripShadow2Path },
 
-			<!-- color bubble -->
-			<path
-				fill="${checkerboardPattern.patternRef}"
-				d="${colorBubblePath}"
-			/>
-			<path
-				fill="${color}"
-				d="${colorBubblePath}"
-			/>
-		`;
+			// Color bubble
+			{ fill: checkerboardPattern.patternRef, d: colorBubblePath },
+			{ fill: colorString, d: colorBubblePath },
+		);
 
-		icon.innerHTML = `
-		<defs>
-			${checkerboardPattern.patternDef}
-		</defs>
-		<g>
-			${ink}
-			${penTip}
-			${grip}
-		</g>
-		`;
+		const icon = document.createElementNS(svgNamespace, 'svg');
+		icon.setAttribute('viewBox', '0 0 100 100');
+
+		const iconMainContent = createSvgElement('g', {
+			children: [ink, penTip, grip].flat(),
+		});
+		const defs = createSvgElement('defs', {
+			children: [checkerboardPattern.patternDefElement()],
+		});
+
+		icon.replaceChildren(defs, iconMainContent);
 		return icon;
 	}
 
@@ -631,7 +614,7 @@ export default class IconProvider {
 			const checkerboardPattern = makeCheckerboardPattern();
 
 			const defs = document.createElementNS(svgNamespace, 'defs');
-			defs.innerHTML = checkerboardPattern.patternDef;
+			defs.appendChild(checkerboardPattern.patternDefElement());
 			icon.appendChild(defs);
 
 			const background = document.createElementNS(svgNamespace, 'g');
@@ -719,7 +702,7 @@ export default class IconProvider {
 			const checkerboardPattern = makeCheckerboardPattern();
 
 			const defs = document.createElementNS(svgNamespace, 'defs');
-			defs.innerHTML = checkerboardPattern.patternDef;
+			defs.appendChild(checkerboardPattern.patternDefElement());
 			icon.appendChild(defs);
 
 			const fluidBackground = document.createElementNS(svgNamespace, 'path');
@@ -979,6 +962,9 @@ export default class IconProvider {
 	 * @returns An object with both the definition of a checkerboard pattern and the syntax to
 	 * reference that pattern. The defs provided by this function should be wrapped within a
 	 * `<defs></defs>` element.
+	 *
+	 * **Note**: This function's return value includes both `patternDefElement` (which returns
+	 * an Element) and a (deprecated) `patternDef` string. Avoid using the `patternDef` result.
 	 */
 	protected makeCheckerboardPattern() {
 		return makeCheckerboardPattern();

packages/js-draw/src/util/createElement.ts
@@ -1,0 +1,113 @@
+type ElementTagNames = keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap;
+
+/**
+ * Maps from known elment tag names to options that can be set with .setAttribute.
+ * New elements/properties should be added as necessary.
+ */
+interface ElementToPropertiesMap {
+	path: {
+		d: string;
+		fill: string;
+		stroke: string;
+		transform: string;
+	};
+	rect: {
+		stroke: string;
+		fill: string;
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		transform: string;
+	};
+	stop: {
+		offset: string;
+		'stop-color': string;
+	};
+	svg: {
+		viewBox: `${number} ${number} ${number} ${number}`;
+	};
+}
+type EmptyObject = Record<never, never>;
+type ElementProperties<Tag extends ElementTagNames> = Tag extends keyof ElementToPropertiesMap
+	? Partial<ElementToPropertiesMap[Tag]>
+	: EmptyObject;
+
+/** Contains options for creating an element with tag = `Tag`. */
+type ElementConfig<Tag extends ElementTagNames> = ElementProperties<Tag> & {
+	id?: string;
+	children?: (HTMLElement | SVGElement)[];
+};
+
+/**
+ * Maps from element tag names (e.g. `Tag='button'`) to the corresponding element type
+ * (e.g. `HTMLButtonElement`).
+ */
+type ElementTagToType<Tag extends ElementTagNames> = Tag extends keyof HTMLElementTagNameMap
+	? HTMLElementTagNameMap[Tag]
+	: Tag extends keyof SVGElementTagNameMap
+		? SVGElementTagNameMap[Tag]
+		: never;
+
+export enum ElementNamespace {
+	Html = 'html',
+	Svg = 'svg',
+}
+
+/**
+ * Shorthand for creating an element with `document.createElement`, then assigning properties.
+ *
+ * Non-HTML elements (e.g. `svg` elements) should use the `elementType` parameter to select
+ * the element namespace.
+ */
+const createElement = <Tag extends ElementTagNames>(
+	tag: Tag,
+	props: ElementConfig<Tag>,
+	elementType: ElementNamespace = ElementNamespace.Html,
+): ElementTagToType<Tag> => {
+	let elem: ElementTagToType<Tag>;
+	if (elementType === ElementNamespace.Html) {
+		elem = document.createElement(tag) as ElementTagToType<Tag>;
+	} else if (elementType === ElementNamespace.Svg) {
+		elem = document.createElementNS('http://www.w3.org/2000/svg', tag) as ElementTagToType<Tag>;
+	} else {
+		throw new Error(`Unknown element type ${elementType}`);
+	}
+
+	for (const [key, value] of Object.entries(props)) {
+		if (key === 'children') continue;
+
+		if (typeof value !== 'string' && typeof value !== 'number') {
+			throw new Error(`Unsupported value type ${typeof value}`);
+		}
+		elem.setAttribute(key, value.toString());
+	}
+
+	if (props.children) {
+		for (const item of props.children) {
+			elem.appendChild(item);
+		}
+	}
+
+	return elem;
+};
+
+export const createSvgElement = <Tag extends keyof SVGElementTagNameMap>(
+	tag: Tag,
+	props: ElementConfig<Tag>,
+) => {
+	return createElement(tag, props, ElementNamespace.Svg);
+};
+
+export const createSvgElements = <Tag extends keyof SVGElementTagNameMap>(
+	tag: Tag,
+	elements: ElementConfig<Tag>[],
+) => {
+	return elements.map((props) => createSvgElement(tag, props));
+};
+
+export const createSvgPaths = (...paths: ElementConfig<'path'>[]) => {
+	return createSvgElements('path', paths);
+};
+
+export default createElement;

packages/material-icons/src/lib.ts
@@ -59,10 +59,18 @@ import Shapes from './icons/Shapes.svg';
 import Draw from './icons/Draw.svg';
 import InkPen from './icons/InkPen.svg';
 import ContentPaste from './icons/ContentPaste.svg';
+import { OpaqueIconType } from './types';
 
-const icon = (data: string) => {
+/**
+ * Converts an icon to an HTML element.
+ *
+ * This function accepts an "opaque" type to avoid unsafely creating icon DOM elements
+ * from untrusted strings.
+ */
+const icon = (data: OpaqueIconType) => {
 	const icon = document.createElement('div');
-	icon.innerHTML = data;
+	// eslint-disable-next-line no-unsanitized/property -- data must not be user-provided (enforced with a custom type).
+	icon.innerHTML = data as unknown as string;
 	return icon.childNodes[0] as SVGElement;
 };
 
@@ -139,7 +147,7 @@ class MaterialIconProvider extends IconProvider {
 		if (style.color.a < 1) {
 			const checkerboard = this.makeCheckerboardPattern();
 			const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
-			defs.innerHTML = checkerboard.patternDef;
+			defs.appendChild(checkerboard.patternDefElement());
 			svg.appendChild(defs);
 
 			const lineBackground = line.cloneNode() as SVGPathElement;

.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
+		'plugin:no-unsanitized/recommended-legacy',
 		'eslint-config-prettier',
 	],
 	// See https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file

docs/demo/icons.ts
@@ -41,11 +41,16 @@ export const makeIconFromText = (text: string) => {
 
 export const makeLocalStorageIcon = () => {
 	const elem = document.createElementNS(svgNamespace, 'svg');
-	const iconFillSStyle = 'style="fill: var(--icon-color);"';
-	elem.innerHTML = `
-		<path d="M 50,10 V 60 H 35 L 55,85 75,60 H 60 V 10 Z" ${iconFillSStyle}/>
-		<path d="m 15,85 v 10 h 85 V 85 Z" ${iconFillSStyle}/>
-	`;
+
+	const makePath = (d: string) => {
+		const path = document.createElementNS(svgNamespace, 'path');
+		path.style.fill = 'var(--icon-color)';
+		path.setAttribute('d', d);
+		elem.appendChild(path);
+	};
+
+	makePath('M 50,10 V 60 H 35 L 55,85 75,60 H 60 V 10 Z');
+	makePath('m 15,85 v 10 h 85 V 85 Z');
 	elem.setAttribute('viewBox', '5 0 100 100');
 
 	return elem;

docs/demo/ui/makeNewImageDialog.ts
@@ -87,10 +87,11 @@ const makeNewImageDialog = (
 		const templateButton = document.createElement('button');
 		templateButton.classList.add('new-image-template-button');
 
-		const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-		icon.innerHTML = svgData;
+		const icon = new Image();
+		icon.classList.add('icon');
+		icon.src = `data:image/svg+xml;utf8,${encodeURIComponent(svgData)}`;
 		const label = document.createElement('div');
-		label.innerText = title;
+		label.textContent = title;
 
 		templateButton.onclick = () => {
 			closeDialogWithStringResult(svgData);

docs/demo/ui/newImageDialog.css
@@ -14,7 +14,11 @@
 }
 
 .new-image-template-button .icon {
-	width: 95%;
+	width: 300px;
+	height: 150px;
+
+	object-position: left top;
+	object-fit: none; /* Do not resize to fit container */
 }
 
 .new-image-template-button:hover {

packages/js-draw/src/SVGLoader/SVGLoader.ts
@@ -700,6 +700,7 @@ export default class SVGLoader implements ImageLoader {
 		const { svgElem, cleanUp } = (() => {
 			// If the user requested an iframe load (the default) try to load with an iframe.
 			// There are some cases (e.g. in a sandboxed iframe) where this doesn't work.
+			// TODO(v2): Use domParserLoad by default.
 			if (!domParserLoad) {
 				try {
 					const sandbox = document.createElement('iframe');
@@ -744,6 +745,7 @@ export default class SVGLoader implements ImageLoader {
 					sandboxDoc.close();
 
 					const svgElem = sandboxDoc.createElementNS('http://www.w3.org/2000/svg', 'svg');
+					// eslint-disable-next-line no-unsanitized/property -- setting innerHTML in a sandboxed document.
 					svgElem.innerHTML = text;
 					sandboxDoc.body.appendChild(svgElem);
 

commit 3b72da77fc0e313dc772d28622ce67c12df5d268

fix(editor): Fix checkerboard pattern not visible for transparent fills
Regression from 0970ab8bed05c4996e388b0730619f5053ea552f

packages/js-draw/src/toolbar/IconProvider.ts
@@ -16,28 +16,26 @@ const svgNamespace = 'http://www.w3.org/2000/svg';
 let checkerboardIdCounter = 0;
 const makeCheckerboardPattern = () => {
 	const id = `checkerboard-${checkerboardIdCounter++}`;
-	const patternDef = `
-		<pattern
-			id='${id}'
-			viewBox='0,0,10,10'
-			width='20%'
-			height='20%'
-			patternUnits='userSpaceOnUse'
-		>
-			<rect x='0' y='0' width='10' height='10' fill='white'/>
-			<rect x='0' y='0' width='5' height='5' fill='gray'/>
-			<rect x='5' y='5' width='5' height='5' fill='gray'/>
-		</pattern>
-	`;
+	const patternElement = createSvgElement('pattern', {
+		id: id,
+		viewBox: '0,0,10,10',
+		width: '20%',
+		height: '20%',
+		patternUnits: 'userSpaceOnUse',
+		children: createSvgElements('rect', [
+			{ x: 0, y: 0, width: 10, height: 10, fill: 'white' },
+			{ x: 0, y: 0, width: 5, height: 5, fill: 'gray' },
+			{ x: 5, y: 5, width: 5, height: 5, fill: 'gray' },
+		]),
+	});
 	const patternRef = `url(#${id})`;
 
 	return {
-		patternDefElement: (): SVGElement => {
-			const element = new DOMParser().parseFromString(patternDef, 'image/svg+xml');
-			return element.firstChild as SVGElement;
-		},
+		patternDefElement: patternElement as SVGElement,
 		// @deprecated use patternDefElement
-		patternDef,
+		get patternDef() {
+			return patternElement.innerHTML;
+		},
 		patternRef,
 	};
 };
@@ -572,7 +570,7 @@ export default class IconProvider {
 			children: [ink, penTip, grip].flat(),
 		});
 		const defs = createSvgElement('defs', {
-			children: [checkerboardPattern.patternDefElement()],
+			children: [checkerboardPattern.patternDefElement],
 		});
 
 		icon.replaceChildren(defs, iconMainContent);
@@ -614,7 +612,7 @@ export default class IconProvider {
 			const checkerboardPattern = makeCheckerboardPattern();
 
 			const defs = document.createElementNS(svgNamespace, 'defs');
-			defs.appendChild(checkerboardPattern.patternDefElement());
+			defs.appendChild(checkerboardPattern.patternDefElement);
 			icon.appendChild(defs);
 
 			const background = document.createElementNS(svgNamespace, 'g');
@@ -702,7 +700,7 @@ export default class IconProvider {
 			const checkerboardPattern = makeCheckerboardPattern();
 
 			const defs = document.createElementNS(svgNamespace, 'defs');
-			defs.appendChild(checkerboardPattern.patternDefElement());
+			defs.appendChild(checkerboardPattern.patternDefElement);
 			icon.appendChild(defs);
 
 			const fluidBackground = document.createElementNS(svgNamespace, 'path');

packages/js-draw/src/util/createElement.ts
@@ -20,6 +20,12 @@ interface ElementToPropertiesMap {
 		height: number;
 		transform: string;
 	};
+	pattern: {
+		viewBox: string;
+		width: string;
+		height: string;
+		patternUnits: 'userSpaceOnUse';
+	};
 	stop: {
 		offset: string;
 		'stop-color': string;

packages/material-icons/src/lib.ts
@@ -147,7 +147,7 @@ class MaterialIconProvider extends IconProvider {
 		if (style.color.a < 1) {
 			const checkerboard = this.makeCheckerboardPattern();
 			const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
-			defs.appendChild(checkerboard.patternDefElement());
+			defs.appendChild(checkerboard.patternDefElement);
 			svg.appendChild(defs);
 
 			const lineBackground = line.cloneNode() as SVGPathElement;