b3bd786a9a1cade41609628ae2f5056ca8bda760
Henry Heino <personalizedrefrigerator@gmail.com>
2024-12-18 12:52:54 -0800

    chore: Add additional TextTool automated test

--- packages/js-draw/src/tools/TextTool.test.ts
+++ packages/js-draw/src/tools/TextTool.test.ts
@@ -4,9 +4,16 @@ import sendPenEvent from '../testing/sendPenEvent';
 import TextTool from './TextTool';
 import { InputEvtType } from '../inputEvents';
 import { Vec2 } from '@js-draw/math';
+import fillInput from '../testing/fillHtmlInput';
+import sendKeyPressRelease from '../testing/sendKeyPressRelease';
+import { TextComponent } from '../lib';
 
 const getTextTool = (editor: Editor) => {
 	return editor.toolController.getMatchingTools(TextTool)[0];
+};
+
+const getTextEditor = (editor: Editor): HTMLTextAreaElement | null => {
+	return editor.getRootElement().querySelector('.textEditorOverlay textarea');
 };
 
 describe('TextTool', () => {
@@ -28,14 +35,33 @@ describe('TextTool', () => {
 		const editor = createEditor();
 		const textTool = getTextTool(editor);
 		textTool.setEnabled(true);
-		const getTextEditor = () => {
-			return editor.getRootElement().querySelector('.textEditorOverlay textarea');
-		};
-		expect(getTextEditor()).toBeFalsy();
+		expect(getTextEditor(editor)).toBeFalsy();
 
 		sendPenEvent(editor, InputEvtType.PointerDownEvt, Vec2.zero);
 		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.zero);
 
-		expect(getTextEditor()).toBeTruthy();
+		expect(getTextEditor(editor)).toBeTruthy();
+	});
+
+	test('pressing enter should finalize editing', () => {
+		const editor = createEditor();
+		const textTool = getTextTool(editor);
+		textTool.setEnabled(true);
+
+		sendPenEvent(editor, InputEvtType.PointerDownEvt, Vec2.zero);
+		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.zero);
+
+		const textArea = getTextEditor(editor);
+		if (!textArea) throw new Error('Could not find the <textarea> element!');
+
+		fillInput(textArea, 'test...');
+		sendKeyPressRelease(textArea, 'Enter');
+
+		const textComponents = editor.image
+			.getAllElements()
+			.filter((element) => element instanceof TextComponent);
+		expect(textComponents).toHaveLength(1);
+
+		expect(textComponents[0].getText()).toBe('test...');
 	});
 });
