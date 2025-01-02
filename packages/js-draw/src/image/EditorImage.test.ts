personalizedrefrigerator/js-draw

commit 4394467f6acbc721ae128a06989c525eaf3c4470

fix(editor): Fix text tool edits existing text in some cases when it shouldn't
This commit fixes an issue with EditorImage.getLeavesIntersectingRegion
-- it could include certain items that **did not** intersect the
provided rectangle. This caused issues with the TextTool when clicking
to start a new text region. Instead of creating a new text region, an
existing one would be edited.

packages/js-draw/src/image/EditorImage.ts
@@ -712,7 +712,7 @@ export class ImageNode {
 
 		while (workList.length > 0) {
 			const current = workList.pop()!;
-			if (current.content !== null) {
+			if (current.content !== null && region.intersects(current.getBBox())) {
 				result.push(current);
 			}
 

packages/js-draw/src/tools/TextTool.test.ts
@@ -6,14 +6,22 @@ import { InputEvtType } from '../inputEvents';
 import { Vec2 } from '@js-draw/math';
 import fillInput from '../testing/fillHtmlInput';
 import sendKeyPressRelease from '../testing/sendKeyPressRelease';
-import { TextComponent } from '../lib';
+import { TextComponent } from '../components/lib';
+import { assertTruthy } from '../util/assertions';
 
 const getTextTool = (editor: Editor) => {
 	return editor.toolController.getMatchingTools(TextTool)[0];
 };
 
 const getTextEditor = (editor: Editor): HTMLTextAreaElement | null => {
-	return editor.getRootElement().querySelector('.textEditorOverlay textarea');
+	return editor.getRootElement().querySelector('.textEditorOverlay textarea:not(.-hiding)');
+};
+
+const getEditorText = (editor: Editor) => {
+	const textComponents = editor.image
+		.getAllElements()
+		.filter((element) => element instanceof TextComponent);
+	return textComponents.map((c) => c.getText()).join('\n');
 };
 
 describe('TextTool', () => {
@@ -52,7 +60,7 @@ describe('TextTool', () => {
 		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.zero);
 
 		const textArea = getTextEditor(editor);
-		if (!textArea) throw new Error('Could not find the <textarea> element!');
+		assertTruthy(textArea);
 
 		fillInput(textArea, 'test...');
 		sendKeyPressRelease(textArea, 'Enter');
@@ -63,5 +71,41 @@ describe('TextTool', () => {
 		expect(textComponents).toHaveLength(1);
 
 		expect(textComponents[0].getText()).toBe('test...');
+	});
+
+	test('clicking away from a text input should finalize editing', () => {
+		const editor = createEditor();
+		const textTool = getTextTool(editor);
+		textTool.setEnabled(true);
+
+		// Create an initial text
+		sendPenEvent(editor, InputEvtType.PointerDownEvt, Vec2.zero);
+		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.zero);
+
+		let textArea = getTextEditor(editor);
+		assertTruthy(textArea);
+
+		fillInput(textArea, 'test!');
+		textArea.dispatchEvent(new FocusEvent('blur'));
+
+		expect(getEditorText(editor)).toBe('test!');
+		expect(textArea.classList.contains('-hiding')).toBe(true);
+
+		// Start a new text elsewhere
+		sendPenEvent(editor, InputEvtType.PointerDownEvt, Vec2.of(1_000, -101));
+		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.of(1_000, -101));
+
+		textArea = getTextEditor(editor);
+		assertTruthy(textArea);
+		expect(textArea.value).toBe('');
+		fillInput(textArea, 'test 2');
+
+		textArea.dispatchEvent(new FocusEvent('blur'));
+
+		sendPenEvent(editor, InputEvtType.PointerDownEvt, Vec2.of(123, 456));
+		sendPenEvent(editor, InputEvtType.PointerUpEvt, Vec2.of(123, 456));
+
+		// Both texts should be added.
+		expect(getEditorText(editor)).toBe('test!\ntest 2');
 	});
 });

packages/js-draw/src/tools/TextTool.ts
@@ -215,13 +215,19 @@ export default class TextTool extends BaseTool {
 			}
 		};
 		this.textInputElem.onblur = () => {
+			const input = this.textInputElem;
+
 			// Delay removing the input -- flushInput may be called within a blur()
 			// event handler
 			const removeInput = false;
-			const input = this.textInputElem;
-
 			this.flushInput(removeInput);
+
 			this.textInputElem = null;
+
+			if (input) {
+				input.classList.add('-hiding');
+			}
+
 			setTimeout(() => {
 				input?.remove();
 			}, 0);

packages/js-draw/src/util/assertions.ts
@@ -1,11 +1,14 @@
+// Note: Arrow functions cannot be used for type assertions. See
+// https://github.com/microsoft/TypeScript/issues/34523
+
 /**
  * Compile-time assertion that a branch of code is unreachable.
  * @internal
  */
-export const assertUnreachable = (key: never): never => {
+export function assertUnreachable(key: never): never {
 	// See https://stackoverflow.com/a/39419171/17055750
 	throw new Error(`Should be unreachable. Key: ${key}.`);
-};
+}
 
 /**
  * Throws an exception if the typeof given value is not a number or `value` is NaN.
@@ -20,47 +23,41 @@ export const assertUnreachable = (key: never): never => {
  *
  *
  */
-export const assertIsNumber = (value: any, allowNaN: boolean = false): value is number => {
+export function assertIsNumber(value: unknown, allowNaN: boolean = false): asserts value is number {
 	if (typeof value !== 'number' || (!allowNaN && isNaN(value))) {
 		throw new Error('Given value is not a number');
-		// return false;
 	}
-
-	return true;
-};
+}
 
 /**
  * Throws if any of `values` is not of type number.
  */
-export const assertIsNumberArray = (
-	values: any[],
+export function assertIsNumberArray(
+	values: unknown[],
 	allowNaN: boolean = false,
-): values is number[] => {
+): asserts values is number[] {
 	if (typeof values !== 'object') {
 		throw new Error('Asserting isNumberArray: Given entity is not an array');
 	}
 
-	if (!assertIsNumber(values['length'])) {
-		return false;
-	}
+	assertIsNumber(values.length);
 
 	for (const value of values) {
-		if (!assertIsNumber(value, allowNaN)) {
-			return false;
-		}
+		assertIsNumber(value, allowNaN);
 	}
-
-	return true;
-};
+}
 
 /**
  * Throws an exception if `typeof value` is not a boolean.
  */
-export const assertIsBoolean = (value: any): value is boolean => {
+export function assertIsBoolean(value: unknown): asserts value is boolean {
 	if (typeof value !== 'boolean') {
 		throw new Error('Given value is not a boolean');
-		// return false;
 	}
+}
 
-	return true;
-};
+export function assertTruthy<T>(value: T | null | undefined | false | 0): asserts value is T {
+	if (!value) {
+		throw new Error(`${JSON.stringify(value)} is not truthy`);
+	}
+}

commit 03c280ee113407dfa2d48b56858375b6ad79c03e

fix(editor): Fix background is hidden when (0,0) is not in view
Regression from 4394467f6acbc721ae128a06989c525eaf3c4470

packages/js-draw/src/image/EditorImage.ts
@@ -684,11 +684,14 @@ export class ImageNode {
 		});
 	}
 
-	public getChildrenOrSelfIntersectingRegion(region: Rect2): ImageNode[] {
-		if (this.content) {
+	public getChildrenOrSelfIntersectingRegion(
+		region: Rect2,
+		isTooSmall?: TooSmallToRenderCheck,
+	): ImageNode[] {
+		if (this.content && this.bbox.intersects(region) && !isTooSmall?.(this.bbox)) {
 			return [this];
 		}
-		return this.getChildrenIntersectingRegion(region);
+		return this.getChildrenIntersectingRegion(region, isTooSmall);
 	}
 
 	/**
@@ -712,11 +715,17 @@ export class ImageNode {
 
 		while (workList.length > 0) {
 			const current = workList.pop()!;
-			if (current.content !== null && region.intersects(current.getBBox())) {
-				result.push(current);
-			}
 
-			workList.push(...current.getChildrenIntersectingRegion(region, isTooSmall));
+			// Split the children into leaves and non-leaves
+			const processed = current.getChildrenOrSelfIntersectingRegion(region, isTooSmall);
+			for (const item of processed) {
+				if (item.content) {
+					result.push(item);
+				} else {
+					// Non-leaves need to be processed
+					workList.push(item);
+				}
+			}
 		}
 
 		return result;
@@ -1157,6 +1166,19 @@ export class RootImageNode extends ImageNode {
 		}
 
 		return result;
+	}
+
+	public override getChildrenOrSelfIntersectingRegion(
+		region: Rect2,
+		_isTooSmall?: TooSmallToRenderCheck,
+	): ImageNode[] {
+		const content = this.getContent();
+		// Fullscreen components always intersect/contain
+		if (content && content.getSizingMode() === ComponentSizingMode.FillScreen) {
+			return [this];
+		}
+
+		return super.getChildrenOrSelfIntersectingRegion(region, _isTooSmall);
 	}
 
 	public override getLeaves(): ImageNode[] {

packages/js-draw/src/image/EditorImage.test.ts
@@ -345,6 +345,17 @@ describe('EditorImage', () => {
 				// Should have a parent
 				expect(image.findParent(testComponent)).not.toBeNull();
 
+				// If set to fill the screen, even a query with a far-away bounding box
+				// should return the element.
+				if (positioning === ComponentSizingMode.FillScreen) {
+					expect(
+						image
+							.getElementsIntersectingRegion(new Rect2(50, 50, 1, 1), true)
+							.includes(testComponent),
+					).toBe(true);
+				}
+
+				// Querying the component's own bounding box should also return results.
 				const elements = image.getElementsIntersectingRegion(
 					// Grow the check region if an empty bbox
 					bbox.maxDimension === 0 ? bbox.grownBy(1) : bbox,